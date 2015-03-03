076bbcb8b4bde115f7d50b9c6021bcdab51c543a
Michael Nelson <michael@eventespresso.com>
2015-03-03 13:54:04 -0800

    Revert "[touch:7554]{t:15}removed the import page and tell folks its disabled for now"
    This reverts commit 7d6dcb446d0ffc663a2d1a4791341adc1c7a6697.

--- caffeinated/admin/extend/events/templates/import_page.template.php
+++ caffeinated/admin/extend/events/templates/import_page.template.php
@@ -1,6 +1,3 @@
 <div class="import-area">
-	<div class="important-notice">
-		<?php _e( 'The import feature has been disabled because of bugs. It is expected to be put back in place soon.', 'event_espresso' );?>
-	</div>
-<?php // echo $form?>
+<?php echo $form?>
 </div>
