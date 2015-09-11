60fcba9bafa8fe782aed1438215f67ceb300ccce
Brent Christensen <brent@eventespresso.com>
2015-09-11 10:54:05 -0700

    add $custom_where_conditions param to constructor then pass to call parent::__construct(); typos and phpdocs; [touch:7537]

--- core/db_models/strategies/EE_CPT_Where_Conditions.strategy.php
+++ core/db_models/strategies/EE_CPT_Where_Conditions.strategy.php
@@ -1,16 +1,36 @@
-<?php
+<?php if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
+	exit( 'No direct script access allowed' );
+}
 
 /*
+ * EE_CPT_Where_Conditions
+ *
  * Strategy specifically for adding where conditions specific to CPT models.
+ *
+ * @package			Event Espresso
+ * @subpackage		core/db_models/strategies/
+ * @author				Michael Nelson
  */
 class EE_CPT_Where_Conditions extends EE_Default_Where_Conditions{
 
 	protected $_post_type;
 	protected $_meta_field;
-	function __construct($post_type, $meta_field_to_chk){
+
+
+
+	/**
+	 * @param array $post_type
+	 * @param string $meta_field_to_chk
+	 * @param array $custom_where_conditions
+	 */
+	function __construct( $post_type, $meta_field_to_chk, $custom_where_conditions = array() ){
 		$this->_post_type = $post_type;
 		$this->_meta_field = $meta_field_to_chk;
+		parent::__construct( $custom_where_conditions );
 	}
+
+
+
 	/**
 	 * Gets the field with the specified column. Note, this function might not work
 	 * properly if two fields refer to columns with the same name on different tables
@@ -24,7 +44,11 @@ class EE_CPT_Where_Conditions extends EE_Default_Where_Conditions{
 				return $field_obj;
 			}
 		}
+		// todo: throw exception if column not found ?  or  return null ?
 	}
+
+
+
 	/**
 	 * At a minimum, we pretty well ALWAYS want to include the post type where querying
 	 * CPT models, otherwise we could get rows which aren't of this post type
@@ -33,14 +57,21 @@ class EE_CPT_Where_Conditions extends EE_Default_Where_Conditions{
 	function _get_minimum_where_conditions(){
 		return array(
 			$this->_get_field_on_column('post_type')->get_name() => $this->_post_type
-				);
+		);
 	}
+
+
+
+	/**
+	 * @return array
+	 * @throws \EE_Error
+	 */
 	protected function _get_default_where_conditions() {
 		//find post_type field
 		$status_field = $this->_get_field_on_column('post_status');
 		return  array_replace_recursive(
-				$this->_get_minimum_where_conditions(),
-				array( $status_field->get_name() => array('NOT IN',array('auto-draft','trash')))
+			$this->_get_minimum_where_conditions(),
+			array( $status_field->get_name() => array('NOT IN',array('auto-draft','trash')))
 		);
 	}
 }
