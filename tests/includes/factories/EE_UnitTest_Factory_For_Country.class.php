3dc8b1a30ecf499855136e0c0d39b51d2ccc26ca
Brent Christensen <brent@eventespresso.com>
2015-12-01 08:53:42 -0800

    add EE_UnitTest_Factory_For_Country class; [touch:9099]

---
+++ tests/includes/factories/EE_UnitTest_Factory_For_Country.class.php
@@ -0,0 +1,59 @@
+<?php if ( ! defined('EVENT_ESPRESSO_VERSION')) { exit('No direct script access allowed'); }
+/**
+ * EE Factory Class for Venues
+ *
+ * @since        4.3.0
+ * @package        Event Espresso
+ * @subpackage    tests
+ *
+ */
+class EE_UnitTest_Factory_For_Country extends EE_UnitTest_Factory_for_Model_Object {
+
+
+	/**
+	 * constructor
+	 *
+	 * @param EE_UnitTest_Factory $factory
+	 * @param array | null        $properties_and_relations
+	 *          pass null (or nothing) to just get the default properties with NO relations
+	 *          or pass empty array for default properties AND relations
+	 *          or non-empty array to override default properties and manually set related objects and their properties,
+	 */
+	public function __construct( $factory, $properties_and_relations = null ) {
+		$this->set_model_object_name( 'Country' );
+		parent::__construct( $factory, $properties_and_relations );
+	}
+
+
+
+	/**
+	 * _set_default_properties_and_relations
+	 *
+	 * @access protected
+	 * @param string $called_class in order to avoid recursive application of relations,
+	 *                             we need to know which class is making this request
+	 * @return void
+	 */
+	protected function _set_default_properties_and_relations( $called_class ) {
+		// set some sensible defaults for this model object
+		if ( empty( $this->_default_properties ) ) {
+			$this->_default_properties = array(
+				'CNT_ISO'       => "US",
+			);
+		}
+		// and set some sensible default relations
+		if ( empty( $this->_default_relations ) ) {
+			$this->_default_relations = array(
+				'State'    => array(),
+				//'Attendee' => array(),
+				//'Venue'    => array(),
+			);
+			$this->_resolve_default_relations( $called_class );
+		}
+	}
+
+
+
+}
+// End of file EE_UnitTest_Factory_For_Venue.class.php
+// Location: /EE_UnitTest_Factory_For_Venue.class.php
