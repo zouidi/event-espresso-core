68e44a662c3b5be540f16da676359083aa116563
Brent Christensen <brent@eventespresso.com>
2015-12-01 08:57:37 -0800

    refactor EE_UnitTest_Factory_For_Payment class; [touch:9099]

--- tests/includes/factories/EE_UnitTest_Factory_For_Payment.class.php
+++ tests/includes/factories/EE_UnitTest_Factory_For_Payment.class.php
@@ -10,83 +10,56 @@
  * @subpackage    tests
  *
  */
-class EE_UnitTest_Factory_For_Payment extends WP_UnitTest_Factory_For_Thing {
+class EE_UnitTest_Factory_For_Payment extends EE_UnitTest_Factory_for_Model_Object {
 
-	public function __construct( $factory = null ) {
-		parent::__construct( $factory );
-		//default args for creating payments.
-		$this->default_generation_definitions = array();
+
+	/**
+	 * constructor
+	 *
+	 * @param EE_UnitTest_Factory $factory
+	 * @param array | null        $properties_and_relations
+	 *          pass null (or nothing) to just get the default properties with NO relations
+	 *          or pass empty array for default properties AND relations
+	 *          or non-empty array to override default properties and manually set related objects and their properties,
+	 */
+	public function __construct( $factory, $properties_and_relations = null ) {
+		$this->set_model_object_name( 'Payment' );
+		parent::__construct( $factory, $properties_and_relations );
 	}
 
 
 
 	/**
-	 * used by factory to create payment object
+	 * _set_default_properties_and_relations
 	 *
-	 * @since 4.3.0
-	 *
-	 * @param array $args Incoming field values to set on the new object
-	 *
-	 * @return EE_Payment|false
+	 * @access protected
+	 * @param string $called_class in order to avoid recursive application of relations,
+	 *                             we need to know which class is making this request
+	 * @return void
 	 */
-	public function create_object( $args ) {
-		//timezone?
-		if ( isset( $args[ 'timezone' ] ) ) {
-			$timezone = $args[ 'timezone' ];
-			unset( $args[ 'timezone' ] );
-		} else {
-			$timezone = null;
+	protected function _set_default_properties_and_relations( $called_class ) {
+		static $counter = 1;
+		// set some sensible defaults for this model object
+		if ( empty( $this->_default_properties ) ) {
+			$this->_default_properties = array(
+				'PAY_timestamp' => time(),
+			);
+			$counter++;
 		}
-		//date_formats?
-		if ( isset( $args[ 'formats' ] ) && is_array( $args[ 'formats' ] ) ) {
-			$formats = $args[ 'formats' ];
-			unset( $args[ 'formats' ] );
-		} else {
-			$formats = array();
+		// and set some sensible default relations
+		if ( empty( $this->_default_relations ) ) {
+			$this->_default_relations = array(
+				'Status' => array(
+					'STS_ID' => EEM_Payment::status_id_pending,
+				),
+				'Transaction'          => array(),
+				'Payment_Method'       => array(),
+				'Registration' 		   => array(),
+				//'Registration_Payment' => array(),
+			);
+			$counter++;
+			$this->_resolve_default_relations( $called_class );
 		}
-		$payment = EE_Payment::new_instance( $args, $timezone, $formats );
-		$paymentID = $payment->save();
-		return $paymentID ? $payment : false;
-	}
-
-
-
-	/**
-	 * Update payment object for given payment
-	 *
-	 * @since 4.3.0
-	 *
-	 * @param int $PMT_ID Payment ID for the payment to update
-	 * @param array $cols_n_data columns and values to change/update
-	 *
-	 * @return EE_Payment|false
-	 */
-	public function update_object( $PMT_ID, $cols_n_data ) {
-		//all the stuff for updating an payment.
-		$payment = EEM_Payment::instance()->get_one_by_ID( $PMT_ID );
-		if ( ! $payment instanceof EE_Payment ) {
-			return null;
-		}
-		foreach ( $cols_n_data as $key => $val ) {
-			$payment->set( $key, $val );
-		}
-		$success = $payment->save();
-		return $success ? $payment : false;
-	}
-
-
-
-	/**
-	 * return the payment object for a given payment ID
-	 *
-	 * @since 4.3.0
-	 *
-	 * @param int $PMT_ID the payment id for the payment to attempt to retrieve
-	 *
-	 * @return mixed null|EE_Payment
-	 */
-	public function get_object_by_id( $PMT_ID ) {
-		return EEM_Payment::instance()->get_one_by_ID( $PMT_ID );
 	}
 
 
