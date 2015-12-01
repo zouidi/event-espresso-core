b25444aafb2c2f160bc3403ab6b76b058e7cbf26
Brent Christensen <brent@eventespresso.com>
2015-12-01 08:54:13 -0800

    add test_EE_UnitTest_Factory_example_usage() but leave commented out; [touch:9099]

--- tests/testcases/espresso_Tests.php
+++ tests/testcases/espresso_Tests.php
@@ -3,7 +3,7 @@
  * Contains test class for espresso.php
  *
  * @since  		4.3.0
- * @package 		Event Espresso
+ * @package 	Event Espresso
  * @subpackage 	tests
  */
 
@@ -12,7 +12,7 @@
  * All tests for the espresso.php file.
  *
  * @since 		4.3.0
- * @package 		Event Espresso
+ * @package 	Event Espresso
  * @subpackage 	tests
  */
 class espresso_Tests extends EE_UnitTestCase {
@@ -94,4 +94,140 @@ class espresso_Tests extends EE_UnitTestCase {
 
 		$this->assertTrue( class_exists( 'EE_Error' ) );
 	}
+
+
+
+	//function test_EE_UnitTest_Factory_example_usage() {
+	//	echo "\n\n espresso_Tests::test_EE_UnitTest_Factory() START";
+	//	$datetimes = array();
+	//	echo "\n\n\n\n CREATE EE_DATETIME WITH DEFAULT PROPERTIES AND ZERO RELATIONS \n";
+	//	$datetimes['EE_Datetime with default properties and zero relations'] = $this->factory->datetime->create_object();
+	//	echo "\n\n\n\n CREATE EE_DATETIME WITH DEFAULT PROPERTIES AND DEFAULT RELATIONS \n";
+	//	$datetimes['EE_Datetime with default properties and default relations'] = $this->factory->datetime_chained->create_object();
+	//	echo "\n\n\n\n SET_PROPERTIES_AND_RELATIONS for CUSTOM EE_Datetime \n";
+	//	$this->factory->datetime->set_properties_and_relations(
+	//		array(
+	//			'DTT_name'        => 'CUSTOM DATETIME NAME',
+	//			'DTT_description' => 'CUSTOM DATETIME DESCRIPTION',
+	//			'DTT_EVT_start'   => time() + ( 14 * DAY_IN_SECONDS ),
+	//			'DTT_EVT_end'     => time() + ( 14.5 * DAY_IN_SECONDS ),
+	//			'DTT_reg_limit'   => 12,
+	//			'DTT_sold'        => 0,
+	//			'Ticket' => array(
+	//				'TKT_ID'   		  => '*',
+	//				'TKT_name' 		  => 'CUSTOM TICKET NAME',
+	//				'TKT_description' => 'CUSTOM TICKET DESCRIPTION',
+	//				'TKT_price' 	  => 10.00,
+	//				'TKT_qty'  		  => 12,
+	//				'TKT_sold' 		  => 0,
+	//				'Price' => array(
+	//					'PRC_name'   => 'CUSTOM BASE PRICE',
+	//					'PRC_desc'   => 'CUSTOM BASE PRICE DESCRIPTION',
+	//					'PRC_amount' => 5.00,
+	//				),
+	//				'Price*' => array(
+	//					'PRT_ID'     => 5,
+	//					'PRC_name'   => 'CUSTOM SURCHARGE 1',
+	//					'PRC_desc'   => 'CUSTOM SURCHARGE 1 DESCRIPTION',
+	//					'PRC_amount' => 2.00,
+	//				),
+	//				'Price**' => array(
+	//					'PRT_ID'     => 5,
+	//					'PRC_name'   => 'CUSTOM SURCHARGE 2',
+	//					'PRC_desc'   => 'CUSTOM SURCHARGE 2 DESCRIPTION',
+	//					'PRC_amount' => 3.00,
+	//				),
+	//			),
+	//		)
+	//	);
+	//	echo "\n\n CREATE EE_DATETIME WITH CUSTOM PROPERTIES AND CUSTOM RELATIONS \n";
+	//	$datetimes['EE_Datetime with custom properties and custom relations'] = $this->factory->datetime->create_object();
+	//	foreach ( $datetimes as $test => $datetime ) {
+	//		if ( $datetime instanceof EE_Datetime ) {
+	//			echo "\n\n\n\n TEST $test";
+	//			echo "\n\n  datetime->ID(): " . $datetime->ID();
+	//			echo "\n  datetime->name(): " . $datetime->name();
+	//			$tickets = $datetime->tickets();
+	//			foreach ( $tickets as $ticket ) {
+	//				if ( $ticket instanceof EE_Ticket ) {
+	//					echo "\n\n   ticket->ID(): " . $ticket->ID();
+	//					echo "\n   ticket->name(): " . $ticket->name();
+	//					echo "\n   ticket->price(): " . $ticket->price();
+	//					$ticket_datetimes = $ticket->datetimes();
+	//					foreach ( $ticket_datetimes as $ticket_datetime ) {
+	//						if ( $ticket_datetime instanceof EE_Datetime ) {
+	//							echo "\n\n    ticket_datetime->ID(): " . $ticket_datetime->ID();
+	//							echo "\n    ticket_datetime->name(): " . $ticket_datetime->name();
+	//						}
+	//					}
+	//
+	//					$prices = $ticket->prices();
+	//					foreach ( $prices as $price ) {
+	//						if ( $price instanceof EE_Price ) {
+	//							echo "\n\n    price->ID(): " . $price->ID();
+	//							echo "\n    price->name(): " . $price->name();
+	//							echo "\n    price->amount(): " . $price->amount();
+	//							$price_type = $price->type_obj();
+	//							if ( $price_type instanceof EE_Price_Type ) {
+	//								echo "\n\n     price_type->ID(): " . $price_type->ID();
+	//								echo "\n     price_type->name(): " . $price_type->name();
+	//								echo "\n     price_type->base_type_name(): " . $price_type->base_type_name();
+	//							}
+	//						}
+	//					}
+	//				}
+	//			}
+	//		}
+	//	}
+	//	echo "\n\n\n\n CREATE EE_Transaction WITH DEFAULT PROPERTIES AND DEFAULT RELATIONS \n";
+	//	$transaction = $this->factory->transaction_chained->create_object();
+	//	if ( $transaction instanceof EE_Transaction ) {
+	//		echo "\n\n transaction->ID(): " . $transaction->ID();
+	//		echo "\n transaction->status_ID(): " . $transaction->status_ID();
+	//		$registrations = $transaction->registrations();
+	//		foreach ( $registrations as $registration  ){
+	//			if ( $registration instanceof EE_Registration ) {
+	//				echo "\n\n   registration->ID(): " . $registration->ID();
+	//				echo "\n   registration->status_ID(): " . $registration->status_ID();
+	//				$attendee = $registration->attendee();
+	//				if ( $attendee instanceof EE_Attendee ) {
+	//					echo "\n\n     attendee->ID(): " . $attendee->ID();
+	//					echo "\n     attendee->full_name(): " . $attendee->full_name();
+	//					echo "\n     attendee->state_name(): " . $attendee->state_name();
+	//					echo "\n     attendee->country_name(): " . $attendee->country_name();
+	//				}
+	//				$ticket = $registration->ticket();
+	//				if ( $ticket instanceof EE_Ticket ) {
+	//					echo "\n\n   ticket->ID(): " . $ticket->ID();
+	//					echo "\n   ticket->name(): " . $ticket->name();
+	//					echo "\n   ticket->price(): " . $ticket->price();
+	//					$ticket_datetimes = $ticket->datetimes();
+	//					foreach ( $ticket_datetimes as $ticket_datetime ) {
+	//						if ( $ticket_datetime instanceof EE_Datetime ) {
+	//							echo "\n\n    ticket_datetime->ID(): " . $ticket_datetime->ID();
+	//							echo "\n    ticket_datetime->name(): " . $ticket_datetime->name();
+	//						}
+	//					}
+	//					$prices = $ticket->prices();
+	//					foreach ( $prices as $price ) {
+	//						if ( $price instanceof EE_Price ) {
+	//							echo "\n\n    price->ID(): " . $price->ID();
+	//							echo "\n    price->name(): " . $price->name();
+	//							echo "\n    price->amount(): " . $price->amount();
+	//							$price_type = $price->type_obj();
+	//							if ( $price_type instanceof EE_Price_Type ) {
+	//								echo "\n\n     price_type->ID(): " . $price_type->ID();
+	//								echo "\n     price_type->name(): " . $price_type->name();
+	//								echo "\n     price_type->base_type_name(): " . $price_type->base_type_name();
+	//							}
+	//						}
+	//					}
+	//				}
+	//			}
+	//		}
+	//	}
+	//	echo "\n\n espresso_Tests::test_EE_UnitTest_Factory() END \n\n";
+	//}
+
 }
+// Location: /tests/testcases/espresso_Tests.php
