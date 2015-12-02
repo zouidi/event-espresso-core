bb17a62bbcdb53002102c34a35725ef9315d44fa
Brent Christensen <brent@eventespresso.com>
2015-12-01 16:02:13 -0800

    remove some debug code; [touch:9099]

--- tests/includes/factories/EE_UnitTest_Factory_For_Datetime.class.php
+++ tests/includes/factories/EE_UnitTest_Factory_For_Datetime.class.php
@@ -19,7 +19,6 @@ class EE_UnitTest_Factory_For_Datetime extends EE_UnitTest_Factory_for_Model_Obj
 	 *        or non-empty array to override default properties and manually set related objects and their properties,
 	 */
 	public function __construct( $factory, $properties_and_relations = null ) {
-		//echo "\n\n " . __LINE__ . ") " . __METHOD__ . "()";
 		$this->set_model_object_name( 'Datetime' );
 		parent::__construct( $factory, $properties_and_relations );
 	}
@@ -35,7 +34,6 @@ class EE_UnitTest_Factory_For_Datetime extends EE_UnitTest_Factory_for_Model_Obj
 	 * @return void
 	 */
 	protected function _set_default_properties_and_relations( $called_class ) {
-		//echo "\n\n " . __LINE__ . ") " . __METHOD__ . "()";
 		// set some sensible defaults for this model object
 		if ( empty( $this->_default_properties ) ) {
 			static $counter = 1;
@@ -53,12 +51,6 @@ class EE_UnitTest_Factory_For_Datetime extends EE_UnitTest_Factory_for_Model_Obj
 				'Ticket' => array(),
 			);
 			$this->_resolve_default_relations( $called_class );
-			//echo "\n\n RESOLVED_RELATIONS for " . __CLASS__ . " ";
-			//echo implode( ' ', array_keys( $this->_default_relations ) ) . ":";
-			//echo " \n  {{ " . __LINE__ . ") " . __METHOD__ . "() }} \n";
-			//var_dump( $resolved_relations );
-			//$this->_default_relations = $resolved_relations;
-			//$this->_default_properties = array_merge( $this->_default_properties, $this->_default_relations );
 		}
 	}
 
