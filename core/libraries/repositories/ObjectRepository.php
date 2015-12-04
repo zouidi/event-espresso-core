a96abffbca5ce4646e79d8edb7efcca123d30807
Brent Christensen <brent@eventespresso.com>
2015-12-04 09:56:17 -0800

    rename file; [touch:8209]

--- core/libraries/repositories/ObjectRepository.php
+++ core/libraries/repositories/ObjectRepository.php
@@ -1,51 +1,148 @@
 <?php
 namespace EventEspresso\Core\Libraries\Repositories;
 
-use core\interfaces\EEI_Repository;
-
 if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
 	exit( 'No direct script access allowed' );
 }
+
+
+
 /**
- * Class EE_Object_Repository
+ * Class ObjectRepository
  *
- * abstract storage entity for unique objects with persistence
- * extends EE_Object_Collection...
- * which extends SplObjectStorage, so therefore implements the
+ * abstract storage entity for unique objects
+ * extends SplObjectStorage so therefore implements the
  * Countable, Iterator, Serializable, and ArrayAccess interfaces
  *
- * @package 	Event Espresso
- * @subpackage 	core
- * @author 		Brent Christensen
- * @since 		4.6.31
+ * @package               Event Espresso
+ * @subpackage            core
+ * @author                Brent Christensen
+ * @since                 4.6.31
  *
  */
-abstract class EE_Object_Repository extends EE_Object_Collection implements EEI_Repository {
+class ObjectRepository extends \SplObjectStorage {
+
 
 	/**
-	 * default persist method called on repository objects if none supplied
-	 * @type string $persist_method
+	 * how to set, get, and utilize object info when retrieving objects
+	 *
+	 * @type ObjectInfoStrategyInterface $object_info_strategy
 	 */
-	protected $persist_method;
+	protected $object_info_strategy;
+
 
 
 	/**
-	 * _call_user_func_array_on_current
+	 * @param ObjectInfoStrategyInterface $object_info_strategy
+	 */
+	function __construct( ObjectInfoStrategyInterface $object_info_strategy = null ) {
+		// so that object info strategy can access objects, rewind, etc
+		$object_info_strategy->setRepository( $this );
+		$this->object_info_strategy = $object_info_strategy instanceof ObjectInfoStrategyInterface
+			? $object_info_strategy : new ObjectInfoSingleKeyStrategy();
+	}
+
+
+
+	/**
+	 * addObject
 	 *
-	 * calls the supplied callback method name on the current repository object,
-	 * an array of arguments can also be supplied that will be passed along to the callback method,
-	 * where each element of the $arguments array corresponds to a parameter for the callback method
-	 * PLZ NOTE: if the first argument of the callback requires an array, for example array( 'key' => 'value' )
-	 * then $arguments needs to be a DOUBLE array ie: array( array( 'key' => 'value' ) )
+	 * attaches an object to the SplObjectStorage
+	 * and sets any supplied data associated with the current iterator entry
+	 * by calling ObjectRepository::setObjectInfo()
 	 *
-	 * @access public
-	 * @param string $callback 		name of method found on object to be called.
-	 * @param array $arguments	arrays of arguments that will be passed to the object's callback method
+	 * @access protected
+	 * @param object $object
+	 * @param mixed  $info
+	 * @return bool
+	 */
+	protected function addObject( $object, $info = null ) {
+		$this->attach( $object );
+		$this->setObjectInfo( $object, $info );
+		return $this->contains( $object );
+	}
+
+
+
+	/**
+	 * hasObject
+	 *
+	 * returns TRUE or FALSE depending on whether the supplied object is within the repository
+	 *
+	 * @access protected
+	 * @param object $object
+	 * @return bool
+	 */
+	protected function hasObject( $object ) {
+		return $this->contains( $object );
+	}
+
+
+
+	/**
+	 * setObjectInfo
+	 *
+	 * Sets the data associated with an object in the SplObjectStorage
+	 * according to the ObjectInfoStrategy
+	 *
+	 * @access protected
+	 * @param object $object
+	 * @param mixed  $info
+	 * @return bool
+	 */
+	public function setObjectInfo( $object, $info = null ) {
+		return call_user_func_array( array( $this->object_info_strategy, 'setObjectInfo' ), array( $object, $info ) );
+	}
+
+
+
+	/**
+	 * getObjectByInfo
+	 *
+	 * finds and returns an object in the repository based on the info that was set using addObject()
+	 *
+	 * @access protected
+	 * @param mixed
+	 * @return null | object
+	 */
+	public function getObjectByInfo( $info ) {
+		return call_user_func_array( array( $this->object_info_strategy, 'getObjectByInfo' ), array( $info ) );
+	}
+
+
+
+	/**
+	 * persistObject
+	 *
+	 * primarily used for saving EE_Base_Class classes to the database,
+	 * but can be supplied with a "persistence callback" that can be used for classes that are not instances of EE_Base_Class,
+	 * or for providing alternate ways to persist an object such as session caching, etc...
+	 * an array of arguments can also be supplied that will be passed along to the object's persistence method
+	 *
+	 * @access protected
+	 * @param object $object
+	 * @param string $persistence_callback name of method found on object that can be used for persisting the object
+	 * @param array  $persistence_arguments arrays of arguments that will be passed to the object's persistence method
 	 * @return bool | int
+	 * @throws \EE_Error
 	 */
-	protected function _call_user_func_array_on_current( $callback = '', $arguments = array() ) {
-		if ( $callback !== '' && method_exists( $this->current(), $callback ) ) {
-			return call_user_func_array( array( $this->current(), $callback ), $arguments );
+	protected function persistObject( $object, $persistence_callback = '', $persistence_arguments = array() ) {
+		if ( $this->contains( $object ) ) {
+			$this->rewind();
+			while ( $this->valid() ) {
+				if ( $object === $this->current() ) {
+					$success = false;
+					if ( method_exists( $object, $persistence_callback ) ) {
+						$success = call_user_func_array( array( $object, $persistence_callback ),
+														 $persistence_arguments );
+					} else if ( $object instanceof \EE_Base_Class ) {
+						$success = $object->save( $persistence_arguments );
+					}
+					$this->rewind();
+					return $success;
+				}
+				$this->next();
+			}
 		}
 		return false;
 	}
@@ -53,67 +150,20 @@ abstract class EE_Object_Repository extends EE_Object_Collection implements EEI_
 
 
 	/**
-	 * _call_user_func_on_all
+	 * removeObject
 	 *
-	 * calls the supplied callback method name on ALL repository objects,
+	 * detaches an object from the SplObjectStorage
 	 *
-	 * @access public
-	 * @param string $callback  name of method found on repository objects to be called
-	 * @return bool | int
+	 * @access protected
+	 * @param $object
+	 * @return void
 	 */
-	protected function _call_user_func_on_all( $callback = '' ) {
-		$success = true;
-		if ( $this->valid() ) {
-			$this->rewind();
-			while ( $this->valid() ) {
-				// any negative result will toggle success to false
-				$success = $this->_call_user_func_array_on_current( $callback ) ? $success : false;
-				$this->next();
-			}
-			$this->rewind();
-		}
-		return $success;
-	}
-
-
-
-	/**
-	 * persist
-	 *
-	 * primarily used for saving EE_Base_Class classes to the database,
-	 * but can be supplied with a "persistence callback" that can be used for classes that are not instances of EE_Base_Class,
-	 * or for providing alternate ways to persist an object such as session caching, etc...
-	 * an array of arguments can also be supplied that will be passed along to the object's persistence method
-	 *
-	 * @access public
-	 * @param string 	$persistence_callback 	name of method found on object that can be used for persisting the object
-	 *                                        								defaults to EE_Object_Repository::$persist_method
-	 * @param array 	$persistence_arguments	arrays of arguments that will be passed to the object's persistence method
-	 * @return bool | int
-	 */
-	public function persist( $persistence_callback = '', $persistence_arguments = array() ) {
-		$persistence_callback = ! empty( $persistence_callback ) ? $persistence_callback : $this->persist_method;
-		return $this->_call_user_func_array_on_current( $persistence_callback, $persistence_arguments );
-	}
-
-
-
-	/**
-	 * persist_all
-	 *
-	 * calls \EE_Object_Repository::persist() on all objects within the repository
-	 *
-	 * @access public
-	 * @param string 	$persistence_callback 		name of method found on object that can be used for persisting the object
-	 * @return bool | int
-	 */
-	public function persist_all( $persistence_callback = '' ) {
-		$persistence_callback = ! empty( $persistence_callback ) ? $persistence_callback : $this->persist_method;
-		return $this->_call_user_func_on_all( $persistence_callback );
+	protected function removeObject( $object ) {
+		$this->detach( $object );
 	}
 
 
 
 }
-// End of file EE_Object_Repository.core.php
-// Location: /core/EE_Object_Repository.core.php
+// End of file EE_Object_Repository.php
+// Location: /core/libraries/repositories/EE_Object_Repository.php
