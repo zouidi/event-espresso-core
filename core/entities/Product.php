2e949845529f3253df94de37cde36f46f452c7b4
Brent Christensen <brent@eventespresso.com>
2015-12-04 10:29:21 -0800

    add placeholder for Product class (will probably be renamed to EE_Product and moved to proper folder); [touch:8209]

---
+++ core/entities/Product.php
@@ -0,0 +1,32 @@
+<?php
+namespace EventEspresso\Core\Entities;
+
+use EventEspresso\Core;
+
+if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
+	exit( 'No direct script access allowed' );
+}
+
+
+
+/**
+ * Class Product
+ *
+ * Description
+ *
+ * @package               Event Espresso
+ * @subpackage            core
+ * @author                Brent Christensen
+ * @since                 $VID:$
+ *
+ */
+class Product extends \EE_Base_Class {
+
+
+
+}
+
+
+
+// End of file Product.php
+// Location: /Product.php
