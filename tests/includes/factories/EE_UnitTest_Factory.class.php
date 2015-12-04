fbe076aa402668a2ca706f2bf2784371d1a00a9f
Brent Christensen <brent@eventespresso.com>
2015-12-04 11:02:46 -0800

    add static $counter property; reinstate named properties for factory classes; use get_class_vars() to intialize factory classes upon construction; refactor get_factory_for_model() to check properties first; [touch:9099]

--- tests/includes/factories/EE_UnitTest_Factory.class.php
+++ tests/includes/factories/EE_UnitTest_Factory.class.php
@@ -12,6 +12,11 @@ class EE_UnitTest_Factory extends WP_UnitTest_Factory {
 
 
 	/**
+	 * @type int
+	 */
+	public static $counter = 0;
+
+	/**
 	 * EE_Test_Factories extend the EE_UnitTest_Factory_for_Model_Object class,
 	 * which extends the WP_UnitTest_Factory_for_Thing abstract class
 	 *
@@ -20,38 +25,173 @@ class EE_UnitTest_Factory extends WP_UnitTest_Factory {
 	 */
 	public $repo = array();
 
+	/**
+	 * @type EE_UnitTest_Factory_For_Event $event
+	 */
+	public $event = null;
 
+	/**
+	 * @type EE_UnitTest_Factory_For_Event $event_chained
+	 */
+	public $event_chained = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Venue $venue
+	 */
+	public $venue = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Venue $venue_chained
+	 */
+	public $venue_chained = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Datetime $datetime
+	 */
+	public $datetime = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Datetime $datetime_chained
+	 */
+	public $datetime_chained = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Ticket $ticket
+	 */
+	public $ticket = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Ticket $ticket_chained
+	 */
+	public $ticket_chained = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Price $price
+	 */
+	public $price = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Price $price_chained
+	 */
+	public $price_chained = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Price_Type $price_type
+	 */
+	public $price_type = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Price_Type $price_type_chained
+	 */
+	public $price_type_chained = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Registration $registration
+	 */
+	public $registration = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Registration $registration_chained
+	 */
+	public $registration_chained = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Transaction $transaction
+	 */
+	public $transaction = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Transaction $transaction_chained
+	 */
+	public $transaction_chained = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Attendee $attendee
+	 */
+	public $attendee = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Attendee $attendee_chained
+	 */
+	public $attendee_chained = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Status $status
+	 */
+	public $status = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Status $status_chained
+	 */
+	public $status_chained = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Payment $payment
+	 */
+	public $payment = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Payment $payment_chained
+	 */
+	public $payment_chained = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_State $state
+	 */
+	public $state = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_State $state_chained
+	 */
+	public $state_chained = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Country $country
+	 */
+	public $country = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Country $country_chained
+	 */
+	public $country_chained = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Line_item $line_item
+	 */
+	public $line_item = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Line_item $line_item_chained
+	 */
+	public $line_item_chained = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Wp_User $wp_user
+	 */
+	public $wp_user = null;
+
+	/**
+	 * @type EE_UnitTest_Factory_For_Wp_User $wp_user_chained
+	 */
+	public $wp_user_chained = null;
 
 
 
 	public function __construct() {
+		//echo "\n\n ************ " . __LINE__ . ") " . __METHOD__ . "() " . spl_object_hash( $this ) . " ************ \n\n ";
 		parent::__construct();
 
-		// simple factories
-		// setup any properties containing various test factory objects.
-
-		$factories = array(
-			'event',
-			'venue',
-			'datetime',
-			'ticket',
-			'price',
-			'price_type',
-			'registration',
-			'transaction',
-			'attendee',
-			'status',
-			'payment',
-			'state',
-			'country',
-			'wp_user',
-			'line_item',
-		);
-		foreach ( $factories as $factory ) {
+		$factories = get_class_vars( 'EE_UnitTest_Factory' );
+		foreach ( $factories as $factory => $value ) {
 			$class = 'EE_UnitTest_Factory_For_' . str_replace( ' ', '_', ucwords( str_replace( '_', ' ', $factory ) ) );
-			if ( class_exists( $class )) {
-				$this->repo[ $factory ]              = new $class( $this );
-				$this->repo[ $factory . '_chained' ] = new $class( $this, array() );
+			if (
+				class_exists( $class )
+				&& property_exists( $this, $factory )
+				&& ! $this->$factory instanceof EE_UnitTest_Factory_for_Model_Object
+			) {
+				$this->$factory = new $class( $this );
+				$factory .= '_chained';
+				$this->$factory = new $class( $this, array() );
 			}
 		}
 
@@ -67,7 +207,11 @@ class EE_UnitTest_Factory extends WP_UnitTest_Factory {
 	 */
 	public function get_factory_for_model( $model_name ) {
 		$model_name = strtolower( rtrim( $model_name, '*' ) );
-		if ( isset( $this->repo[ $model_name ] ) ) {
+		//echo "\n\n " . __LINE__ . ") " . __METHOD__ . "() <br />";
+		//echo "\n MODEL_NAME: " . $model_name . "<br />";
+		if ( property_exists( $this, $model_name ) ) {
+			return $this->$model_name;
+		} else if ( isset( $this->repo[ $model_name ] ) ) {
 			return $this->repo[ $model_name ];
 		}
 		return null;
@@ -83,6 +227,8 @@ class EE_UnitTest_Factory extends WP_UnitTest_Factory {
 	 * @return \EE_UnitTest_Factory_for_Model_Object
 	 */
 	public function construct_generic_factory_for_model( $model_name ) {
+		//echo "\n\n " . __LINE__ . ") " . __METHOD__ . "()";
+		//echo "\n MODEL_NAME: " . $model_name . "\n";
 		$model_name = strtolower( rtrim( $model_name, '*' ) );
 		$this->repo[ $model_name ] = new EE_UnitTest_Factory_For_Generic_Model( $model_name, $this );
 		$this->repo[ $model_name . '_chained' ] = new EE_UnitTest_Factory_For_Generic_Model( $model_name, $this, array() );
