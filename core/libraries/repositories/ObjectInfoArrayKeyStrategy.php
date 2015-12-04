b600f6c1848d4f7b6b546785d2158ec6a20b64f1
Brent Christensen <brent@eventespresso.com>
2015-12-04 10:22:15 -0800

    refactor to work with Collections; add setCurrentByInfo() method; [touch:8209]

--- core/libraries/repositories/ObjectInfoArrayKeyStrategy.php
+++ core/libraries/repositories/ObjectInfoArrayKeyStrategy.php
@@ -2,6 +2,8 @@
 
 namespace EventEspresso\Core\Libraries\Repositories;
 
+use core\interfaces\EEI_Collection;
+
 if ( ! defined('EVENT_ESPRESSO_VERSION')) {
 	exit('No direct script access allowed');
 }
@@ -23,84 +25,118 @@ if ( ! defined('EVENT_ESPRESSO_VERSION')) {
  * @since 				$VID:$
  *
  */
- class ObjectInfoArrayKeyStrategy implements ObjectInfoStrategyInterface {
+class ObjectInfoArrayKeyStrategy implements ObjectInfoStrategyInterface {
 
 
-	 /**
-	  * @type ObjectRepository $object_repository
-	  */
-	 protected $object_repository;
-
-
-
-	 /**
-	  * @param ObjectRepository $object_repository
-	  */
-	 public function setRepository( ObjectRepository $object_repository ) {
-		 $this->object_repository = $object_repository;
-	 }
+	/**
+	* @type ObjectRepository $object_collection
+	*/
+	protected $object_collection;
 
 
 
-	 /**
-	  * setObjectInfo
-	  *
-	  * Sets the data associated with an object in the SplObjectStorage
-	  * if no $info is supplied, then the spl_object_hash() is used
-	  *
-	  * @access protected
-	  * @param object $object
-	  * @param mixed  $info
-	  * @return bool
-	  */
-	 public function setObjectInfo( $object, $info = null ) {
-		 $info = is_array( $info ) ? $info : array( 'ID' => spl_object_hash( $object ) );
-		 $this->object_repository->rewind();
-		 while ( $this->object_repository->valid() ) {
-			 if ( $object == $this->object_repository->current() ) {
-				 $this->object_repository->setInfo( $info );
-				 $this->object_repository->rewind();
-				 return;
-			 }
-			 $this->object_repository->next();
-		 }
-	 }
+	/**
+	* @param EEI_Collection $object_collection
+	*/
+	public function setCollection( EEI_Collection $object_collection ) {
+		$this->object_collection = $object_collection;
+	}
 
 
 
-	 /**
-	  * getObjectByInfo
-	  *
-	  * finds and returns an object in the repository based on the info that was set using addObject()
-	  *
-	  * @access protected
-	  * @param array $array_of_key_value_pairings
-	  * @return null|object
-	  */
-	 public function getObjectByInfo( $array_of_key_value_pairings ) {
-		 $this->object_repository->rewind();
-		 while ( $this->object_repository->valid() ) {
-			 $currentInfo = $this->object_repository->getInfo();
-			 if ( ! is_array( $currentInfo ) ) {
-				 $this->object_repository->next();
-				 continue;
-			 }
-			 $found = true;
-			 foreach ( $array_of_key_value_pairings as $key => $value ) {
-				 if ( ! isset( $currentInfo[ $key ] ) || $currentInfo[ $key ] !== $value ) {
-					 $found = false;
-					 break;
-				 }
-			 }
-			 if ( ! $found ) {
-				 continue;
-			 }
-			 $object = $this->object_repository->current();
-			 $this->object_repository->rewind();
-			 return $object;
-		 }
-		 return null;
-	 }
+	/**
+	* setObjectInfo
+	*
+	* Sets the data associated with an object in the SplObjectStorage
+	* if no $info is supplied, then the spl_object_hash() is used
+	*
+	* @access protected
+	* @param object $object
+	* @param mixed  $info
+	* @return bool
+	*/
+	public function setObjectInfo( $object, $info = null ) {
+		$info = is_array( $info ) ? $info : array( 'ID' => spl_object_hash( $object ) );
+		$this->object_collection->rewind();
+		while ( $this->object_collection->valid() ) {
+			if ( $object == $this->object_collection->current() ) {
+				$this->object_collection->setInfo( $info );
+				$this->object_collection->rewind();
+				return;
+			}
+			$this->object_collection->next();
+		}
+	}
+
+
+
+	/**
+	* getObjectByInfo
+	*
+	* finds and returns an object in the repository based on the info that was set using addObject()
+	*
+	* @access protected
+	* @param array $array_of_key_value_pairings
+	* @return null|object
+	*/
+	public function getObjectByInfo( $array_of_key_value_pairings ) {
+		$this->object_collection->rewind();
+		while ( $this->object_collection->valid() ) {
+			$currentInfo = $this->object_collection->getInfo();
+			if ( ! is_array( $currentInfo ) ) {
+				$this->object_collection->next();
+				continue;
+			}
+			$found = true;
+			foreach ( $array_of_key_value_pairings as $key => $value ) {
+				if ( ! isset( $currentInfo[ $key ] ) || $currentInfo[ $key ] !== $value ) {
+					$found = false;
+					break;
+				}
+			}
+			if ( ! $found ) {
+				continue;
+			}
+			$object = $this->object_collection->current();
+			$this->object_collection->rewind();
+			return $object;
+		}
+		return null;
+	}
+
+
+
+	/**
+	 * setCurrentByInfo
+	 *
+	 * advances pointer to the object whose info matches that which was provided
+	 *
+	 * @access public
+	 * @param $array_of_key_value_pairings
+	 * @return void
+	 */
+	public function setCurrentByInfo( $array_of_key_value_pairings ) {
+		$this->object_collection->rewind();
+		while ( $this->object_collection->valid() ) {
+			$currentInfo = $this->object_collection->getInfo();
+			if ( ! is_array( $currentInfo ) ) {
+				$this->object_collection->next();
+				continue;
+			}
+			$found = true;
+			foreach ( $array_of_key_value_pairings as $key => $value ) {
+				if ( ! isset( $currentInfo[ $key ] ) || $currentInfo[ $key ] !== $value ) {
+					$found = false;
+					break;
+				}
+			}
+			if ( ! $found ) {
+				$this->object_collection->next();
+			}
+			break;
+		}
+	}
+
 
 
 }
