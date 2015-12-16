d70b2dc9f0a5f1eaf26fc43bd688cb9b5177177f
Brent Christensen <brent@eventespresso.com>
2015-12-15 20:47:18 -0800

    refactor object creation using new methods; [touch:9099]

--- tests/testcases/core/db_models/EEM_Answer_Test.php
+++ tests/testcases/core/db_models/EEM_Answer_Test.php
@@ -18,29 +18,64 @@ if (!defined('EVENT_ESPRESSO_VERSION')) {
  */
 class EEM_Answer_Test extends EE_UnitTestCase{
 	function test_get_registration_question_answer_object(){
-		$r = $this->new_model_obj_with_dependencies('Registration');
-		$a = $this->new_model_obj_with_dependencies('Answer',array('REG_ID'=>$r->ID()));
-		$q = EEM_Question::instance()->get_one_by_ID($a->question_ID());
+		/** @type EE_Registration $r */
+		$r = $this->new_model_obj_with_dependencies(
+			'Registration',
+			array(
+				'Answer' => array(
+					'Question' => array()
+				),
+			)
+		);
+		$answers = $r->answers();
+		$a = reset( $answers );
+		$q = EEM_Question::instance()->get_one_by_ID( $a->question_ID() );
 		$this->assertNotNull($q);
 		$this->assertEquals( $a, EEM_Answer::instance()->get_registration_question_answer_object( $r, $a->question_ID() ) );
 	}
 	function test_get_attendee_property_answer_value(){
+		/** @type EE_Registration $r */
 		$r = $this->new_model_obj_with_dependencies( 'Registration', array( 'Attendee' => array() ) );
-		global $wpdb;
-		$att = EEM_Attendee::reset()->get_one_by_ID( $r->attendee_ID() );
+		EEM_Attendee::reset()->get_one_by_ID( $r->attendee_ID() );
 		$this->assertEquals( $r->attendee()->fname(), EEM_Answer::instance()->get_attendee_property_answer_value( $r,  EEM_Attendee::system_question_fname ) );
 	}
 	public function test_get_answer_value_to_question(){
-		$r = $this->new_model_obj_with_dependencies('Registration', array( 'Attendee' => array() ) );
-		$a = $this->new_model_obj_with_dependencies('Answer',array('REG_ID'=>$r->ID()));
-		$this->assertEquals($a->value(),EEM_Answer::instance()->get_answer_value_to_question($r,$a->question_ID(),false));
-		$this->assertEquals($r->attendee()->fname(),EEM_Answer::instance()->get_answer_value_to_question($r,  EEM_Question::instance()->get_Question_ID_from_system_string( EEM_Attendee::system_question_fname ) ) );
+		/** @type EE_Registration $r */
+		$r = $this->new_model_obj_with_dependencies(
+			'Registration',
+			array(
+				'Answer' => array(
+					'Question' => array()
+				),
+				'Attendee' => array(),
+			)
+		);
+		$answers = $r->answers();
+		$a = reset( $answers );
+		$this->assertEquals(
+			$a->value(),
+			EEM_Answer::instance()->get_answer_value_to_question( $r, $a->question_ID(), false )
+		);
+		$this->assertEquals(
+			$r->attendee()->fname(),
+			EEM_Answer::instance()->get_answer_value_to_question(
+				$r,
+				EEM_Question::instance()->get_Question_ID_from_system_string(
+					EEM_Attendee::system_question_fname
+				)
+			)
+		);
 	}
 	/**
 	 * @group 8237
 	 */
 	public function test_set_ANS_value__with_unsafe_html(){
-		$a = $this->new_model_obj_with_dependencies( 'Answer', array( 'ANS_value' => '<img src=x onerror=prompt(document.cookie)><img src=x onerror=prompt(/XSS/)><img src=x onerror=prompt(1)>') );
+		$a = $this->new_model_obj_with_dependencies(
+			'Answer',
+			array(
+				'ANS_value' => '<img src=x onerror=prompt(document.cookie)><img src=x onerror=prompt(/XSS/)><img src=x onerror=prompt(1)>'
+			)
+		);
 		$this->assertEquals( '', $a->get_raw( 'ANS_value' ) );
 	}
 
