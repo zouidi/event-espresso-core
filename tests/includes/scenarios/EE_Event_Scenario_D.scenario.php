5ca8f40642e7c06f35a4d308fe76535c9387bbba
Brent Christensen <brent@eventespresso.com>
2015-12-15 20:52:18 -0800

    create tickets seprately then use IDs in other object property args; verified then adjusted expected results because above change revealed that it was incorrect; [touch:9099]

--- tests/includes/scenarios/EE_Event_Scenario_D.scenario.php
+++ tests/includes/scenarios/EE_Event_Scenario_D.scenario.php
@@ -5,9 +5,9 @@ if (!defined('EVENT_ESPRESSO_VERSION'))
 /**
  * This scenario creates an event that has:
  * - Three Datetimes
- *      - D1 - reg limit 5
- *      - D2 - reg limit 20
- *      - D3 - reg limit 12
+ *      - D1 - reg limit 5 		( TA, TB, TC )
+ *      - D2 - reg limit 20    	( TA, TB, TD )
+ *      - D3 - reg limit 12 	( TA, TC, TD )
  * - Four Tickets
  *      - TA - qty 5 	( D1, D2, D3 )
  *      - TB - qty 5 	( D1, D2 )
@@ -16,7 +16,7 @@ if (!defined('EVENT_ESPRESSO_VERSION'))
  *
  *  MAX SELLOUT:
  *        5 TA tickets for D1 ( D1 sold out + TA, TB, & TC sold out )
- *        10 TD tickets for D3 ( TD sold out )
+ *        7 TD tickets for D2 ( D2 sold out + TD sold out )
  *        ( D2 + D3 sold out because ALL tickets sold out )
  *
  * @package    Event Espresso
@@ -33,13 +33,17 @@ class EE_Event_Scenario_D extends EE_Test_Scenario {
 
 	protected function _set_up_expected(){
 		$this->_expected_values = array(
-			'total_available_spaces' => 15,
-			'total_remaining_spaces' => 15
+			'total_available_spaces' => 12,
+			'total_remaining_spaces' => 12
 		);
 	}
 
 
 	protected function _set_up_scenario(){
+		$TKT_A = $this->_eeTest->factory->ticket->create_object( array( 'TKT_name' => 'Ticket A', 'TKT_qty' => 5 ) );
+		$TKT_B = $this->_eeTest->factory->ticket->create_object( array( 'TKT_name' => 'Ticket B', 'TKT_qty' => 5 ) );
+		$TKT_C = $this->_eeTest->factory->ticket->create_object( array( 'TKT_name' => 'Ticket C', 'TKT_qty' => 5 ) );
+		$TKT_D = $this->_eeTest->factory->ticket->create_object( array( 'TKT_name' => 'Ticket D', 'TKT_qty' => 10 ) );
 		$event = $this->generate_objects_for_scenario(
 			array(
 				'Event' => array(
@@ -48,48 +52,39 @@ class EE_Event_Scenario_D extends EE_Test_Scenario {
 						'DTT_name'      => 'Datetime 1',
 						'DTT_reg_limit' => 5,
 						'Ticket'        => array(
-							'TKT_name' => 'Ticket A',
-							'TKT_qty'  => 5,
+							'TKT_ID' => $TKT_A->ID()
 						),
 						'Ticket*'       => array(
-							'TKT_name' => 'Ticket B',
-							'TKT_qty'  => 5,
+							'TKT_ID' => $TKT_B->ID()
 						),
 						'Ticket**'      => array(
-							'TKT_name' => 'Ticket C',
-							'TKT_qty'  => 5,
+							'TKT_ID' => $TKT_C->ID()
 						),
 					),
 					'Datetime*' => array(
 						'DTT_name'      => 'Datetime 2',
 						'DTT_reg_limit' => 20,
 						'Ticket'        => array(
-							'TKT_name' => 'Ticket A',
-							'TKT_qty'  => 5,
+							'TKT_ID' => $TKT_A->ID()
 						),
 						'Ticket*'       => array(
-							'TKT_name' => 'Ticket B',
-							'TKT_qty'  => 5,
+							'TKT_ID' => $TKT_B->ID()
 						),
 						'Ticket**'       => array(
-							'TKT_name' => 'Ticket D',
-							'TKT_qty'  => 10,
+							'TKT_ID' => $TKT_D->ID()
 						),
 					),
 					'Datetime**' => array(
 						'DTT_name'      => 'Datetime 3',
 						'DTT_reg_limit' => 12,
 						'Ticket'        => array(
-							'TKT_name' => 'Ticket A',
-							'TKT_qty'  => 5,
+							'TKT_ID' => $TKT_A->ID()
 						),
 						'Ticket*'       => array(
-							'TKT_name' => 'Ticket C',
-							'TKT_qty'  => 5,
+							'TKT_ID' => $TKT_C->ID()
 						),
 						'Ticket**' => array(
-							'TKT_name' => 'Ticket D',
-							'TKT_qty'  => 10,
+							'TKT_ID' => $TKT_D->ID()
 						),
 					),
 				),
