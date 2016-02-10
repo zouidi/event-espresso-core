86e87e76e7bc804edab2e68b9b3dd3ccf4d44a7d
Brent Christensen <brent@eventespresso.com>
2016-02-10 09:46:58 -0800

    use load_module() method; [touch:9334]

--- core/EED_Module.module.php
+++ core/EED_Module.module.php
@@ -99,7 +99,7 @@ abstract class EED_Module extends EE_Configurable {
 	protected static function get_instance( $module_name = '' ) {
 		$module_name = ! empty( $module_name ) ? $module_name : get_called_class();
 		if ( ! isset(  EE_Registry::instance()->modules->{$module_name} ) || ! EE_Registry::instance()->modules->{$module_name} instanceof EED_Module ) {
-			EE_Registry::instance()->add_module( $module_name );
+			EE_Registry::instance()->load_module( $module_name );
 		}
 		return EE_Registry::instance()->get_module( $module_name );
 	}
