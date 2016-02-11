a480925abba72de0f7fd3730692c8e94d46794c0
Brent Christensen <brent@eventespresso.com>
2016-02-11 10:23:45 -0800

    add InvalidModuleResponseDataException class; [touch:9334]

---
+++ modules/InvalidModuleResponseDataException.php
@@ -0,0 +1,25 @@
+<?php
+namespace EventEspresso\modules;
+
+if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
+	exit( 'No direct script access allowed' );
+}
+
+
+
+/**
+ * Class InvalidModuleResponseDataException
+ *
+ * @package       Event Espresso
+ * @subpackage    core
+ * @author        Brent Christensen
+ * @since         4.8
+ *
+ */
+class InvalidModuleResponseDataException extends \EE_Error {
+
+
+
+}
+// End of file InvalidModuleResponseDataException.php
+// Location: /InvalidModuleResponseDataException.php
