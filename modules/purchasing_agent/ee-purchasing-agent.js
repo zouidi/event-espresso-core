e2f45dfe684965ddbbac3825a3fe5561b321348c
Brent Christensen <brent@eventespresso.com>
2016-02-15 22:02:33 -0800

    add javascript file for PurchasingAgentForm; [touch:8837]

--- modules/purchasing_agent/ee-purchasing-agent.js
+++ modules/purchasing_agent/ee-purchasing-agent.js
@@ -1,44 +1,62 @@
 jQuery(document).ready( function($) {
 
+	var PurchasingAgent = {
 
-	/**
-	 *  @function doAjax
-	 */
-	var displayPurchasingAgentInfo = function() {
-		if ( ! purchasing_agent.length > 0 ) {
-			purchasing_agent = $( '#ee_reg_qstn-purchasing_agent' );
-		}
-		var sync_info_notice = $( '#ee-attendee-sync-info-notice-dv' );
-			var billing_form = $( '#purchaser-billing-form' );
-			var agent = purchasing_agent.val();
-			console.log( JSON.stringify( 'purchasing_agent.val(): ' + agent, null, 4 ) );
-			console.log( JSON.stringify( 'sync_info_notice.length: ' + sync_info_notice.length, null, 4 ) );
-			console.log( JSON.stringify( 'billing_form.length: ' + billing_form.length, null, 4 ) );
+		/**
+		 *  @function displayInfo
+		 */
+		displayInfo : function () {
+			var agent                  = $( 'input:radio[name="ee_purchasing_agent"]:checked' ).val();
+			var additional_info_notice = $( '#ee-additional-attendee-info-notice-dv' );
+			var separate_info_notice   = $( '#ee-additional-separate-info-notice-dv' );
+			var billing_form           = $( '#purchaser-billing-form' );
+			//console.log( JSON.stringify( 'purchasing_agent.val(): ' + agent, null, 4 ) );
+			//console.log( JSON.stringify( 'additional_info_notice.length: ' + additional_info_notice.length, null, 4 ) );
+			//console.log( JSON.stringify( 'separate_info_notice.length: ' + separate_info_notice.length, null, 4 ) );
+			//console.log( JSON.stringify( 'billing_form.length: ' + billing_form.length, null, 4 ) );
 			if ( agent === 'attendee' ) {
-				sync_info_notice.show();
-				billing_form.hide();
+				additional_info_notice.show();
+				separate_info_notice.hide();
+				PurchasingAgent.hideBillingForm( billing_form );
 			} else if ( agent === 'separate' ) {
-				sync_info_notice.hide();
-				billing_form.show();
+				additional_info_notice.hide();
+				separate_info_notice.show();
+				PurchasingAgent.displayBillingForm( billing_form );
 			}
+		},
+
+		/**
+		 *  @function hideBillingForm
+		 */
+		hideBillingForm : function ( billing_form ) {
+			billing_form.hide();
+			billing_form.find( 'input' ).addClass( 'ee-do-not-validate' );
+			billing_form.find( 'select' ).addClass( 'ee-do-not-validate' );
+		},
+
+		/**
+		 *  @function displayBillingForm
+		 */
+		displayBillingForm : function ( billing_form ) {
+			billing_form.show();
+			billing_form.find( 'input' ).removeClass( 'ee-do-not-validate' );
+			billing_form.find( 'select' ).removeClass( 'ee-do-not-validate' );
+		}
+
+
 	};
 
 
 	/**
-	 * define dropdown input for selecting the purchasing agent
-	 */
-	var purchasing_agent = $( '#ee_reg_qstn-purchasing_agent' );
-	/**
 	 * listener for changes to ee_reg_qstn-purchasing_agent select box
 	 */
-	purchasing_agent.on( 'change', function() {
-		displayPurchasingAgentInfo();
+	$( 'input:radio[name="ee_purchasing_agent"]' ).on( 'click', function() {
+		PurchasingAgent.displayInfo();
 	});
-
 
 	/**
 	 * set initial state
 	 */
-	displayPurchasingAgentInfo();
+	PurchasingAgent.displayInfo();
 
 });
