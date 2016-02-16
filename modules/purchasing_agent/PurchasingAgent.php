44ea782e8602960ceed818b2f3c05f97791b2b0d
Brent Christensen <brent@eventespresso.com>
2016-02-15 22:01:35 -0800

    add initial shell for PurchasingAgent class; [touch:8837]

---
+++ modules/purchasing_agent/PurchasingAgent.php
@@ -0,0 +1,27 @@
+<?php
+namespace EventEspresso\modules\purchasing_agent;
+
+if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
+	exit( 'No direct script access allowed' );
+}
+
+
+
+/**
+ * Class PurchasingAgent
+ *
+ * Description
+ *
+ * @package       Event Espresso
+ * @subpackage    core
+ * @author        Brent Christensen
+ * @since         $VID:$
+ *
+ */
+class PurchasingAgent {
+
+
+
+}
+// End of file PurchasingAgent.php
+// Location: /PurchasingAgent.php
