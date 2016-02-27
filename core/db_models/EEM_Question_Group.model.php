fd1da5c52c11923dd78ceb15ffb8e1661f84d6c3
Brent Christensen <brent@eventespresso.com>
2016-02-26 16:12:43 -0800

    update model with new fields; [touch:9413]

--- core/db_models/EEM_Question_Group.model.php
+++ core/db_models/EEM_Question_Group.model.php
@@ -1,62 +1,58 @@
 <?php if ( ! defined('EVENT_ESPRESSO_VERSION')) exit('No direct script access allowed');
 /**
- * Event Espresso
- *
- * Event Registration and Management Plugin for WordPress
- *
- * @ package			Event Espresso
- * @ author				Seth Shoultes
- * @ copyright		(c) 2008-2011 Event Espresso  All Rights Reserved.
- * @ license			http://eventespresso.com/support/terms-conditions/   * see Plugin Licensing *
- * @ link					http://www.eventespresso.com
- * @ version		 	4.0
- *
- * ------------------------------------------------------------------------
- *
  * Question Group Model
  *
  * @package			Event Espresso
  * @subpackage		includes/models/
- * @author				Michael Nelson
+ * @author			Michael Nelson
  *
  * ------------------------------------------------------------------------
  */
 require_once ( EE_MODELS . 'EEM_Soft_Delete_Base.model.php' );
 require_once( EE_CLASSES . 'EE_Question_Group.class.php');
 class EEM_Question_Group extends EEM_Soft_Delete_Base {
+
 	const system_personal = 1;
 	const system_address = 2;
+
   	// private instance of the Attendee object
 	protected static $_instance = NULL;
-        
-        
+
+
 	protected function __construct( $timezone = NULL ) {
+
 		$this->singular_item = __('Question Group','event_espresso');
 		$this->plural_item = __('Question Groups','event_espresso');
 
 		$this->_tables = array(
 			'Question_Group'=>new EE_Primary_Table('esp_question_group','QSG_ID')
 		);
+
 		$this->_fields = array(
 			'Question_Group'=>array(
 				'QSG_ID'=>new EE_Primary_Key_Int_Field('QSG_ID', __('Question Group ID','event_espresso')),
 				'QSG_name'=>new EE_Plain_Text_Field('QSG_name', __('Question Group Name','event_espresso'), false, ''),
 				'QSG_identifier'=>new EE_Plain_Text_Field('QSG_identifier', __('Text ID for question Group','event_espresso'), false, ''),
 				'QSG_desc'=>new EE_Full_HTML_Field('QSG_desc', __('Description of Question Group','event_espresso'), true, ''),
+				'QSG_parent' => new EE_Integer_Field('QSG_parent',__('The Question Group that this is a child of.','event_espresso'),true,null),
+				'QSG_html_name' => new EE_Plain_Text_Field('QSG_html_name',__('Form Section Name','event_espresso'),true,null),
+				'QSG_html_id' => new EE_Plain_Text_Field('QSG_html_id',__('HTML CSS id for question Group','event_espresso'),true,null),
+				'QSG_html_class' => new EE_Plain_Text_Field('QSG_html_class',__('HTML CSS class for question Group','event_espresso'),true,null),
+				'QSG_html_content' => new EE_Plain_Text_Field('QSG_html_content',__('Actual content if this is an HTML Form Section','event_espresso'),true,null),
 				'QSG_order'=>new EE_Integer_Field('QSG_order', __('Order in which to show the question group','event_espresso'), true, 0),
-				'QSG_show_group_name'=>new EE_Boolean_Field('QSG_show_group_name', __('Flag indicating whether to show the group\'s name on the registration page','event_espresso'), false, true),
-				'QSG_show_group_desc'=>new EE_Boolean_Field('QSG_show_group_desc', __('Flag indicating whether to show the group\s description on the registration page','event_espresso'), false, false),
 				'QSG_wp_user' => new EE_WP_User_Field('QSG_wp_user', __('Question Group Creator ID', 'event_espresso'), FALSE ),
 				'QSG_system'=>new EE_Integer_Field('QSG_system', __('Indicate IF this is a system group and if it is what system group it corresponds to.','event_espresso'), false, 0),
 				'QSG_deleted'=>new EE_Trashed_Flag_Field('QSG_deleted', __('Flag indicating this question group was deleted','event_espresso'), false, false)
 			)
 		);
+
 		$this->_model_relations = array(
-			'Question'=>new EE_HABTM_Relation('Question_Group_Question'),
+			'Question'=>new EE_Has_Many_Relation(),
 			'Event'=>new EE_HABTM_Relation('Event_Question_Group'),
 			'Event_Question_Group'=>new EE_Has_Many_Relation(),
 			'WP_User' => new EE_Belongs_To_Relation(),
 		);
+
 		//this model is generally available for reading
 		$this->_cap_restriction_generators[ EEM_Base::caps_read ] = new EE_Restriction_Generator_Public();
 		$this->_cap_restriction_generators[ EEM_Base::caps_read_admin ] = new EE_Restriction_Generator_Reg_Form('QSG_system');
