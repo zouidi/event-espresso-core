d3d52a358b4fa93f251bb554e393bcd068b1d62a
Brent Christensen <brent@eventespresso.com>
2016-03-01 20:58:33 -0800

    formatting; [touch:9413]

--- core/libraries/form_sections/inputs/EE_Yes_No_Input.input.php
+++ core/libraries/form_sections/inputs/EE_Yes_No_Input.input.php
@@ -11,9 +11,14 @@ class EE_Yes_No_Input extends EE_Select_Input{
 	/**
 	 * @param array $options
 	 */
-	function __construct( $options = array()){
-		$select_options = array(true=>  __("Yes", "event_espresso"),false=>  __("No", "event_espresso"));
-
-		parent::__construct($select_options,$options);
+	function __construct( $options = array() ) {
+		$select_options = array(
+			true  => __( "Yes", "event_espresso" ),
+			false => __( "No", "event_espresso" )
+		);
+		parent::__construct( $select_options, $options );
 	}
+
+
+
 }
