d22eea6a4f84dff021b3c08dacf9b9aecf9a1e3d
Brent Christensen <brent@eventespresso.com>
2016-03-01 21:06:03 -0800

    add optional_validation_strategies() method; [touch:9413]

--- core/libraries/form_sections/inputs/EE_Text_Input.input.php
+++ core/libraries/form_sections/inputs/EE_Text_Input.input.php
@@ -25,4 +25,28 @@ class EE_Text_Input extends EE_Form_Input_Base{
 
 
 
+	/**
+	 * list of possible validation strategies that *could* be applied to this input
+	 *
+	 * @return array
+	 */
+	public static function optional_validation_strategies() {
+		return array(
+			//'credit_card' => 'EE_Credit_Card_Validation_Strategy',
+			//'email'       => 'EE_Email_Validation_Strategy',
+			//'enum'        => 'EE_Enum_Validation_Strategy',
+			//'float'       => 'EE_Float_Validation_Strategy',
+			'int'         => 'EE_Int_Validation_Strategy',
+			//'full_html'   => 'EE_Full_HTML_Validation_Strategy',
+			//'many_valued' => 'EE_Many_Valued_Validation_Strategy',
+			'max_length'  => 'EE_Max_Length_Validation_Strategy',
+			'min_length'  => 'EE_Min_Length_Validation_Strategy',
+			'plaintext'   => 'EE_Plaintext_Validation_Strategy',
+			'required'    => 'EE_Required_Validation_Strategy',
+			'simple_html' => 'EE_Simple_HTML_Validation_Strategy',
+			'text'        => 'EE_Text_Validation_Strategy',
+			//'url'         => 'EE_URL_Validation_Strategy',
+		);
+	}
+
 }
