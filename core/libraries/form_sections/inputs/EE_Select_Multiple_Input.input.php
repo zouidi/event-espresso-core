a14aa7da40757eec98d11979a08df1315e201f49
Brent Christensen <brent@eventespresso.com>
2016-03-01 21:07:16 -0800

    formatting and comments; [touch:9413]

--- core/libraries/form_sections/inputs/EE_Select_Multiple_Input.input.php
+++ core/libraries/form_sections/inputs/EE_Select_Multiple_Input.input.php
@@ -14,11 +14,12 @@ class EE_Select_Multiple_Input extends EE_Form_Input_With_Options_Base{
 	 */
 	public function __construct( $answer_options, $input_settings = array() ) {
 		$this->_set_display_strategy( new EE_Select_Multiple_Display_Strategy() );
-		$this->_add_validation_strategy( new EE_Many_Valued_Validation_Strategy( array( new EE_Enum_Validation_Strategy( isset( $input_settings[ 'validation_error_message' ] ) ? $input_settings[ 'validation_error_message' ] : NULL ) )));
-		$this->_multiple_selections = TRUE;
+		$this->_multiple_selections = true;
 		parent::__construct( $answer_options, $input_settings );
 	}
 
+
+
 }
 
-// End of file EE_HABTM_Input.input.php
+// End of file EE_Select_Multiple_Input.input.php
