749f52fb131dcfd4e15c2f08bf8537b088a69f75
Brent Christensen <brent@eventespresso.com>
2016-03-01 21:06:30 -0800

    change default validation to Simple HTML; add optional_validation_strategies() method; [touch:9413]

--- core/libraries/form_sections/inputs/EE_Text_Area_Input.input.php
+++ core/libraries/form_sections/inputs/EE_Text_Area_Input.input.php
@@ -48,14 +48,43 @@ class EE_Text_Area_Input extends EE_Form_Input_Base{
 	/**
 	 * @param array $options_array
 	 */
-	public function __construct($options_array = array()) {
-		$this->_set_display_strategy(new EE_Text_Area_Display_Strategy());
-		$this->_set_normalization_strategy(new EE_Text_Normalization());
+	public function __construct( $options_array = array() ) {
+		$this->_set_display_strategy( new EE_Text_Area_Display_Strategy() );
+		$this->_set_normalization_strategy( new EE_Text_Normalization() );
 		//by default we use the plaintext validation. If you want something else,
 		//just remove it after the input is constructed :P using EE_Form_Input_Base::remove_validation_strategy()
-		$this->_add_validation_strategy( new EE_Plaintext_Validation_Strategy() );
-		parent::__construct($options_array);
+		$this->_add_validation_strategy( new EE_Simple_HTML_Validation_Strategy() );
+		parent::__construct( $options_array );
 	}
+
+
+
+	/**
+	 * list of possible validation strategies that *could* be applied to this input
+	 *
+	 * @return array EE_Enum_Validation_Strategy
+	 */
+	public static function optional_validation_strategies() {
+		return array(
+			//'credit_card' => 'EE_Credit_Card_Validation_Strategy',
+			//'email'       => 'EE_Email_Validation_Strategy',
+			//'enum' => 'EE_Enum_Validation_Strategy',
+			//'float'       => 'EE_Float_Validation_Strategy',
+			//'int'        => 'EE_Int_Validation_Strategy',
+			'full_html'   => 'EE_Full_HTML_Validation_Strategy',
+			//'many_valued' => 'EE_Many_Valued_Validation_Strategy',
+			'max_length' => 'EE_Max_Length_Validation_Strategy',
+			'min_length' => 'EE_Min_Length_Validation_Strategy',
+			'plaintext'   => 'EE_Plaintext_Validation_Strategy',
+			'required'   => 'EE_Required_Validation_Strategy',
+			'simple_html' => 'EE_Simple_HTML_Validation_Strategy',
+			'text'        => 'EE_Text_Validation_Strategy',
+			//'url'         => 'EE_URL_Validation_Strategy',
+		);
+	}
+
+
+
 }
 
 // End of file EE_Text_Area.input.php
