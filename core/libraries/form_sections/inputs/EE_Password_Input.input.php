fd2a0fbfbd7551b0662ee1d19828dd6b1d4322b6
Brent Christensen <brent@eventespresso.com>
2016-03-02 09:44:01 -0800

    formatting; [touch:9413]

--- core/libraries/form_sections/inputs/EE_Password_Input.input.php
+++ core/libraries/form_sections/inputs/EE_Password_Input.input.php
@@ -16,7 +16,7 @@ class EE_Password_Input extends EE_Form_Input_Base{
 		$this->_set_display_strategy(new EE_Text_Input_Display_Strategy('password'));
 		$this->_set_normalization_strategy(new EE_Text_Normalization());
 		parent::__construct($input_settings);
-		$this->set_html_class( $this->html_class() . 'password' );
+		$this->set_html_class( $this->html_class() . ' password' );
 	}
 
 
