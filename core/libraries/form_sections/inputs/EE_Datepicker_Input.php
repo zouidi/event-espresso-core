e84b2e8dde7666e810b3d51f14321d5125032c2b
Brent Christensen <brent@eventespresso.com>
2016-03-01 21:20:11 -0800

    formatting; [touch:9413]

--- core/libraries/form_sections/inputs/EE_Datepicker_Input.php
+++ core/libraries/form_sections/inputs/EE_Datepicker_Input.php
@@ -15,12 +15,18 @@ class EE_Datepicker_Input extends EE_Form_Input_Base{
 		$this->_set_display_strategy( new EE_Text_Input_Display_Strategy( 'datepicker' ) );
 		$this->_set_normalization_strategy( new EE_Text_Normalization() );
 		//we could do better for validation, but at least verify its plaintext
-		$this->_add_validation_strategy( new EE_Plaintext_Validation_Strategy( isset( $input_settings[ 'validation_error_message' ] ) ? $input_settings[ 'validation_error_message' ] : NULL ) );
+		$this->_add_validation_strategy(
+			new EE_Plaintext_Validation_Strategy(
+				isset( $input_settings[ 'validation_error_message' ] )
+					? $input_settings[ 'validation_error_message' ]
+					: null
+			)
+		);
 		parent::__construct( $input_settings );
 		$this->set_html_class( $this->html_class() . ' datepicker' );
 		// add some style and make it dance
-		add_action( 'wp_enqueue_scripts', array( 'EE_Datepicker_Input', 'enqueue_styles_and_scripts' ));
-		add_action( 'admin_enqueue_scripts', array( 'EE_Datepicker_Input', 'enqueue_styles_and_scripts' ));
+		add_action( 'wp_enqueue_scripts', array( 'EE_Datepicker_Input', 'enqueue_styles_and_scripts' ) );
+		add_action( 'admin_enqueue_scripts', array( 'EE_Datepicker_Input', 'enqueue_styles_and_scripts' ) );
 	}
 
 
@@ -36,6 +42,7 @@ class EE_Datepicker_Input extends EE_Form_Input_Base{
 		wp_register_style( 'espresso-ui-theme', EE_GLOBAL_ASSETS_URL . 'css/espresso-ui-theme/jquery-ui-1.10.3.custom.min.css', array(), EVENT_ESPRESSO_VERSION, TRUE );
 		wp_enqueue_style( 'espresso-ui-theme');
 	}
+
 
 
 
