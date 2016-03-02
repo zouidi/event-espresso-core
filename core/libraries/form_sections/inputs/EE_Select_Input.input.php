e53865248a8eb2d499f0cb2d42bc8f36830cd3fa
Brent Christensen <brent@eventespresso.com>
2016-03-01 21:08:34 -0800

    don't add EE_Enum_Validation_Strategy since it's being done by parent class; [touch:9413]

--- core/libraries/form_sections/inputs/EE_Select_Input.input.php
+++ core/libraries/form_sections/inputs/EE_Select_Input.input.php
@@ -17,9 +17,10 @@ class EE_Select_Input extends EE_Form_Input_With_Options_Base{
 	 * @param array $input_settings
 	 */
 	function __construct( $answer_options, $input_settings = array() ){
-		$this->_set_display_strategy( new EE_Select_Display_Strategy( $answer_options ));
-		$this->_add_validation_strategy( new EE_Enum_Validation_Strategy( isset( $input_settings[ 'validation_error_message' ] ) ? $input_settings[ 'validation_error_message' ] : NULL ) );
+		$this->_set_display_strategy( new EE_Select_Display_Strategy( $answer_options ) );
 		parent::__construct( $answer_options, $input_settings );
 	}
+
+
 
 }
