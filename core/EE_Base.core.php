e385195042dd980dcf6b5e892100d03d7b62cdc4
Brent Christensen <brent@eventespresso.com>
2016-03-24 15:04:17 -0700

    fix formatting; [touch:9492]

--- core/EE_Base.core.php
+++ core/EE_Base.core.php
@@ -1,39 +1,71 @@
-<?php if ( ! defined('EVENT_ESPRESSO_VERSION')) exit('No direct script access allowed');
+<?php
+if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
+	exit( 'No direct script access allowed' );
+}
 /**
- * Event Espresso
- *
- * Event Registration and Management Plugin for WordPress
- *
- * @ package			Event Espresso
- * @ author			Seth Shoultes
- * @ copyright		(c) 2008-2011 Event Espresso  All Rights Reserved.
- * @ license			http://eventespresso.com/support/terms-conditions/   * see Plugin Licensing *
- * @ link					http://www.eventespresso.com
- * @ version		 	4.0
- *
- * ------------------------------------------------------------------------
  *
  * EE_BASE
  *
  * @package			Event Espresso
  * @subpackage	core/
- * @author				Brent Christensen 
- *
- * ------------------------------------------------------------------------
+ * @author				Brent Christensen
  */
-class EE_BASE {
+class EE_Base {
 
 	/**
-	 *		@ override magic methods
-	 *		@ return void
-	 */	
-	public function __get($a) { return FALSE; }
-	public function __set($a,$b) { return FALSE; }
-	public function __isset($a) { return FALSE; }
-	public function __unset($a) { return FALSE; }
-	public function __clone() { return FALSE; }
-	public function __wakeup() { return FALSE; }
-	public function __destruct() { return FALSE; }
+	 * @override magic methods
+	 * @param $a
+	 * @param $b
+	 * @return bool
+	 */
+	public function __set( $a, $b ) {
+		return false;
+	}
+
+
+
+	/**
+	 * @param $a
+	 * @return bool
+	 */
+	public function __get( $a ) {
+		return false;
+	}
+
+
+
+	/**
+	 * @param $a
+	 * @return bool
+	 */
+	public function __isset( $a ) {
+		return false;
+	}
+
+
+
+	/**
+	 * @param $a
+	 * @return bool
+	 */
+	public function __unset( $a ) {
+		return false;
+	}
+
+
+
+	public function __clone() {
+	}
+
+
+
+	public function __wakeup() {
+	}
+
+
+
+	public function __destruct() {
+	}
 
 }
 // End of file EE_BASE.core.php
