6ffec01e0c27caeff52163d9c58ae14be4b88bcb
Mike Nelson <michael@eventespresso.com>
2016-04-04 12:50:07 -0700

    [touch:9536]{t:30}actually viewing users from other sites is really cap related anyway

--- core/db_models/strategies/EE_Restriction_Generator_WP_User.strategy.php
+++ core/db_models/strategies/EE_Restriction_Generator_WP_User.strategy.php
@@ -21,14 +21,25 @@ class EE_Restriction_Generator_WP_User extends EE_Restriction_Generator_Base{
 	 * @return \EE_Default_Where_Conditions
 	 */
 	protected function _generate_restrictions() {
-
-		return array(
+		$restrictions =  array(
 			//if they can't access users, they can still access themselves
 			EE_Restriction_Generator_Base::get_cap_name( $this->model(), $this->action() ) => new EE_Default_Where_Conditions( array(
 				EE_Default_Where_Conditions::user_field_name_placeholder => EE_Default_Where_Conditions::current_user_placeholder
 			)),
 
 		);
+		//if its multisite things get more complicated
+		if( is_multisite() ) {
+			global $wpdb;
+			//add a default where condition so we only ever return users for the current blog (on multisite)
+			$restrictions[ EE_Restriction_Generator_Base::get_default_restrictions_cap() ] = new EE_Default_Where_Conditions( 
+					array( 
+						'WP_User_Meta.meta_key' => $wpdb->get_blog_prefix() . 'capabilities',
+						'WP_User_Meta.meta_value' => array( 'IS_NOT_NULL' )
+					)
+				);
+		}
+		return $restrictions;
 	}
 }
 
