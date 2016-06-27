55a12237606b21af5d654d26a2a64d638f091aa8
Brent Christensen <brent@eventespresso.com>
2016-06-27 16:04:10 -0700

    remove unnecessary factory methods; [touch:9856]

--- core/db_classes/EE_Ticket_Price.class.php
+++ core/db_classes/EE_Ticket_Price.class.php
@@ -30,27 +30,6 @@
  */
 class EE_Ticket_Price extends EE_Base_Class {
 
-	/**
-	 * @param array  $props_n_values
-	 * @param string $timezone
-	 * @return EE_Ticket_Price|mixed
-	 */
-	public static function new_instance( $props_n_values = array(), $timezone = '', $date_formats = array() ) {
-		$has_object = parent::_check_for_object( $props_n_values, __CLASS__, $timezone, $date_formats );
-		return $has_object ? $has_object : new self( $props_n_values, FALSE, $timezone, $date_formats );
-	}
-
-
-
-	/**
-	 * @param array  $props_n_values
-	 * @param string $timezone
-	 * @return EE_Ticket_Price
-	 */
-	public static function new_instance_from_db( $props_n_values = array(), $timezone = '' ) {
-		return new self( $props_n_values, TRUE, $timezone );
-	}
-
 
 
 }
