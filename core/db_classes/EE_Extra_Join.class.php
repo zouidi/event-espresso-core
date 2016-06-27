3e70ca0206805b2ccb29b331ffaa0bc59ecf62c8
Brent Christensen <brent@eventespresso.com>
2016-06-27 15:54:30 -0700

    remove unnecessary factory methods; [touch:9856]

--- core/db_classes/EE_Extra_Join.class.php
+++ core/db_classes/EE_Extra_Join.class.php
@@ -17,26 +17,5 @@
  */
 class EE_Extra_Join extends EE_Base_Class {
 
-	/**
-	 * @param array $props_n_values
-	 * @param null  $timezone
-	 * @return EE_Extra_Join|mixed
-	 */
-	public static function new_instance( $props_n_values = array(), $timezone = NULL ) {
-		$has_object = parent::_check_for_object( $props_n_values, __CLASS__, $timezone );
-		return $has_object ? $has_object : new self( $props_n_values, FALSE, $timezone );
-	}
-
-
-
-	/**
-	 * @param array $props_n_values
-	 * @param null  $timezone
-	 * @return EE_Extra_Join
-	 */
-	public static function new_instance_from_db ( $props_n_values = array(), $timezone = NULL ) {
-		return new self( $props_n_values, TRUE, $timezone );
-	}
-
 }
 //end class EE_Extra_Join
