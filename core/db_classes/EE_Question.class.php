debb485b66015eecdbb37e173d8b5414a84ccbaa
Brent Christensen <brent@eventespresso.com>
2016-06-27 15:59:16 -0700

    remove unnecessary factory methods; [touch:9856]

--- core/db_classes/EE_Question.class.php
+++ core/db_classes/EE_Question.class.php
@@ -28,31 +28,6 @@
  */
 class EE_Question extends EE_Soft_Delete_Base_Class implements EEI_Duplicatable {
 
-	/**
-	 *
-	 * @param array $props_n_values  incoming values
-	 * @param string $timezone  incoming timezone (if not set the timezone set for the website will be
-	 *                          		used.)
-	 * @param array $date_formats  incoming date_formats in an array where the first value is the
-	 *                             		    date_format and the second value is the time format
-	 * @return EE_Question
-	 */
-	public static function new_instance( $props_n_values = array(), $timezone = null, $date_formats = array() ) {
-		$has_object = parent::_check_for_object( $props_n_values, __CLASS__, $timezone, $date_formats );
-		return $has_object ? $has_object : new self( $props_n_values, false, $timezone, $date_formats );
-	}
-
-
-
-	/**
-	 * @param array $props_n_values  incoming values from the database
-	 * @param string $timezone  incoming timezone as set by the model.  If not set the timezone for
-	 *                          		the website will be used.
-	 * @return EE_Question
-	 */
-	public static function new_instance_from_db( $props_n_values = array(), $timezone = null ) {
-		return new self( $props_n_values, TRUE, $timezone );
-	}
 
 
 
@@ -553,7 +528,7 @@ class EE_Question extends EE_Soft_Delete_Base_Class implements EEI_Duplicatable 
 					$this,
 					$registration,
 					$answer
-				);				
+				);
 				$result = new EE_State_Select_Input( $state_options, $input_constructor_args );
 				break;
 			// Country Dropdown
