f24e6a2bc62fbe2852279f20d316349e240879ac
Brent Christensen <brent@eventespresso.com>
2016-06-27 15:51:47 -0700

    remove unnecessary factory methods; [touch:9856]

--- core/db_classes/EE_Country.class.php
+++ core/db_classes/EE_Country.class.php
@@ -26,25 +26,6 @@
  */
 class EE_Country extends EE_Base_Class {
 
-	/**
-	 * @param array $props_n_values
-	 * @return EE_Country|mixed
-	 */
-	public static function new_instance( $props_n_values = array() ) {
-		$has_object = parent::_check_for_object( $props_n_values, __CLASS__ );
-		return $has_object ? $has_object : new self( $props_n_values );
-	}
-
-
-
-	/**
-	 * @param array $props_n_values
-	 * @return EE_Country
-	 */
-	public static function new_instance_from_db( $props_n_values = array() ) {
-		return new self( $props_n_values, TRUE );
-	}
-
 
 
 
