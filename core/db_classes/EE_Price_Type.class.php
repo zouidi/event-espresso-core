94aad688ed5befcd21cd64871604c93e174d1f29
Brent Christensen <brent@eventespresso.com>
2016-06-27 15:58:11 -0700

    remove unnecessary factory methods; [touch:9856]

--- core/db_classes/EE_Price_Type.class.php
+++ core/db_classes/EE_Price_Type.class.php
@@ -28,24 +28,7 @@
  */
 class EE_Price_Type extends EE_Soft_Delete_Base_Class {
 
-	/**
-	 * @param array $props_n_values
-	 * @return EE_Price_Type
-	 */
-	public static function new_instance( $props_n_values = array() ) {
-		$has_object = parent::_check_for_object( $props_n_values, __CLASS__ );
-		return $has_object ? $has_object : new self( $props_n_values );
-	}
 
-
-
-	/**
-	 * @param array $props_n_values
-	 * @return EE_Price_Type
-	 */
-	public static function new_instance_from_db( $props_n_values = array() ) {
-		return new self( $props_n_values, TRUE );
-	}
 
 
 
