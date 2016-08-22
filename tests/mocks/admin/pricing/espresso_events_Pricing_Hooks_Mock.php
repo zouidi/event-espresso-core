6b43526b479192e6ceb656e9b6383cb858084e03
Brent Christensen <brent@eventespresso.com>
2016-08-22 16:41:40 -0700

    phpdocs; [touch:9998]

--- tests/mocks/admin/pricing/espresso_events_Pricing_Hooks_Mock.php
+++ tests/mocks/admin/pricing/espresso_events_Pricing_Hooks_Mock.php
@@ -3,6 +3,11 @@
 if (!defined('EVENT_ESPRESSO_VERSION'))
 	exit('No direct script access allowed');
 
+
+require_once( EE_CAFF_PATH . 'admin/new/pricing/espresso_events_Pricing_Hooks.class.php' );
+
+
+
 /**
  *
  * espresso_events_Pricing_Hooks_mock
@@ -13,10 +18,13 @@ if (!defined('EVENT_ESPRESSO_VERSION'))
  * @since  4.6
  *
  */
-require_once( EE_CAFF_PATH . 'admin/new/pricing/espresso_events_Pricing_Hooks.class.php' );
 class espresso_events_Pricing_Hooks_Mock extends espresso_events_Pricing_Hooks {
 
-	public function __construct( $admin_page = '' ) {
+	/**
+	 * constructor
+	 * @param EE_Admin_Page $admin_page the calling admin_page_object
+	 */
+	public function __construct( EE_Admin_Page $admin_page = null ) {
 		$admin_page = ! $admin_page instanceof EE_Admin_Page ? new Admin_Mock_Valid_Admin_Page() : $admin_page;
 		parent::__construct( $admin_page );
 	}
@@ -36,14 +44,23 @@ class espresso_events_Pricing_Hooks_Mock extends espresso_events_Pricing_Hooks {
 
 
 
-
-
+	/**
+	 * @param $evt_obj
+	 * @param $data
+	 * @return \EE_Datetime[]
+	 */
 	public function update_dtts( $evt_obj, $data ) {
 		return $this->_update_dtts( $evt_obj, $data );
 	}
 
 
 
+	/**
+	 * @param $evtobj
+	 * @param $saved_dtts
+	 * @param $data
+	 * @return \EE_Ticket[]
+	 */
 	public function update_tkts( $evtobj, $saved_dtts, $data ) {
 		return $this->_update_tkts( $evtobj, $saved_dtts, $data );
 	}
