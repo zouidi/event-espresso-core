24776ddf8f2a152a06bafd6d4273fc21d6d64738
Brent Christensen <brent@eventespresso.com>
2016-08-24 11:38:13 -0700

    add debugging output; add bootstrap_unit_tests() function and move relevent logic into it; [touch:9998]

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,53 +6,57 @@
  * @package 		Event Espresso
  * @subpackage 	Tests
  */
+// if you get fatal errors or memory overruns or other nasty fun stuff like that during the unit tests,
+// then set EE_UNIT_TEST_DEBUG to true (bool) and you'll get a list of test case classes being run and other debug info
+define( 'EE_UNIT_TEST_DEBUG', false );   // true    false
+if ( EE_UNIT_TEST_DEBUG ) {
+	echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+	echo "\n ************************************************************************************************************** ";
+	echo "\n ******************************************** BOOTSTRAP UNIT TESTS ********************************************";
+	echo "\n ************************************************************************************************************** \n\n\n";
+	echo "\n " . __LINE__ . ") " . basename( __FILE__ ) . "()";
+}
+
 require( dirname( __FILE__ ) . '/includes/define-constants.php' );
-// load PSR4 autoloader
-require_once( EE_PLUGIN_DIR . 'core/Psr4Autoloader.php' );
 if ( ! file_exists( WP_TESTS_DIR . '/includes/functions.php' ) ) {
 	die( "The WordPress PHPUnit test suite could not be found.\n" );
 }
-
 require_once WP_TESTS_DIR . '/includes/functions.php';
-
 function _install_and_load_event_espresso() {
 	require EE_TESTS_DIR . 'includes/loader.php';
 }
 tests_add_filter( 'muplugins_loaded', '_install_and_load_event_espresso' );
-
 require WP_TESTS_DIR . '/includes/bootstrap.php';
-require_once( EE_CORE . 'EE_Dependency_Map.core.php' );
-require_once( EE_CORE . 'request_stack' . DS . 'EE_Request.core.php' );
-require_once( EE_CORE . 'request_stack' . DS . 'EE_Response.core.php' );
-EE_Dependency_Map::instance( new EE_Request( $_GET, $_POST, $_COOKIE ), new EE_Response() );
-add_filter(
-	'FHEE__EE_Registry____construct___class_abbreviations',
-	function( $class_abbreviations = array() ) {
-		$class_abbreviations['EE_Session_Mock'] = 'SSN';
-		return $class_abbreviations;
-	}
-);
-add_filter(
-	'FHEE__EE_Registry__load_core__core_paths',
-	function( $core_paths = array() ) {
-		$core_paths[] = EE_TESTS_DIR . 'mocks' . DS . 'core' . DS;
-		return $core_paths;
-	}
-);
-EE_Dependency_Map::register_dependencies(
-	'EE_Session_Mock',
-	array( 'EE_Encryption' => EE_Dependency_Map::load_from_cache )
-);
-EE_Dependency_Map::register_class_loader( 'Session_Mock' );
-EE_Registry::instance()->SSN = EE_Registry::instance()->load_core( 'EE_Session_Mock' );
-
 //Load the EE_specific testing tools
 require EE_TESTS_DIR . 'includes/EE_UnitTestCase.class.php';
 
-require_once EE_TESTS_DIR . 'mocks/addons/eea-new-addon/eea-new-addon.php';
-remove_action( 'AHEE__EE_System__load_espresso_addons', 'load_espresso_new_addon' );
-//save wpdb queries in case we want to know what queries ran during a test
-define( 'SAVEQUERIES', TRUE );
+bootstrap_unit_tests();
+
+function bootstrap_unit_tests() {
+	if ( EE_UNIT_TEST_DEBUG ) {
+		echo "\n----------------------------------------------------------------------------------------";
+		echo "\n ---------------------------------- BOOTSTRAPPING EE ---------------------------------- ";
+		echo "\n----------------------------------------------------------------------------------------\n";
+	}
+	add_filter(
+		'FHEE__EE_Load_Espresso_Core___load_dependency_map__dependency_map',
+		array( 'EE_UnitTestCase', 'load_dependency_map' )
+	);
+	add_filter(
+		'FHEE__EE_Load_Espresso_Core___load_registry__registry',
+		array( 'EE_UnitTestCase', 'load_registry' )
+	);
+	// prepare to run activation for first request
+	delete_option( 'espresso_db_update' );
+	// here we go
+	\EE_UnitTestCase::set_EE_Bootstrap( new \EE_Bootstrap() );
+	remove_action( 'AHEE__EE_System__load_espresso_addons', 'load_espresso_new_addon' );
+	do_action( 'plugins_loaded' );
+	do_action( 'init' );
+	if ( EE_UNIT_TEST_DEBUG ) {
+		echo "\n --------------------------------- BOOTSTRAP COMPLETE --------------------------------- \n\n";
+	}
+}
 
 /**
  * redefining wp_mail function here as a mock for our tests.  Has to be done early
