943a5d7b0e5c5b44e024f522c0fcff500d4ff13d
Brent Christensen <brent@eventespresso.com>
2016-09-20 20:35:25 -0700

    remove old scripts loading code outside class; call _load_script_stages() during construction; fix class phpdocs; [touch:9413]

--- core/data_migration_scripts/EE_DMS_Core_4_6_0.dms.php
+++ core/data_migration_scripts/EE_DMS_Core_4_6_0.dms.php
@@ -1,43 +1,24 @@
 <?php
 /**
+ * Class EE_DMS_Core_4_6_0
  * meant to convert DBs between 4.6 and 4.6
  * mostly just
  * -move payment methods from EE_Config into a separate table just for them
- */
-//make sure we have all the stages loaded too
-//unfortunately, this needs to be done upon INCLUSION of this file,
-//instead of construction, because it only gets constructed on first page load
-//(all other times it gets resurrected from a wordpress option)
-$stages = glob(EE_CORE.'data_migration_scripts/4_6_0_stages/*');
-$class_to_filepath = array();
-foreach($stages as $filepath){
-	$matches = array();
-	preg_match('~4_6_0_stages/(.*).dmsstage.php~',$filepath,$matches);
-	$class_to_filepath[$matches[1]] = $filepath;
-}
-//give addons a chance to autoload their stages too
-$class_to_filepath = apply_filters('FHEE__EE_DMS_4_6_0__autoloaded_stages',$class_to_filepath);
-EEH_Autoloader::register_autoloader($class_to_filepath);
-
-
-
-
-
-/**
- * Class EE_DMS_Core_4_6_0
  *
- * @package            Event Espresso
+ * @package       Event Espresso
  * @subpackage    core
- * @author                Mike Nelson
- * @since                4.6.0
- *
+ * @author        Mike Nelson
+ * @since         4.6.0
  */
 class EE_DMS_Core_4_6_0 extends EE_Data_Migration_Script_Base{
 
 	/**
 	 * return EE_DMS_Core_4_6_0
+	 *
+	 * @throws \EE_Error
 	 */
 	public function __construct() {
+		$this->_load_script_stages();
 		$this->_pretty_name = __("Data Migration to Event Espresso 4.6.0.P", "event_espresso");
 		$this->_priority = 10;
 		$this->_migration_stages = array(
@@ -609,8 +590,8 @@ class EE_DMS_Core_4_6_0 extends EE_Data_Migration_Script_Base{
 
 			$SQL = "SELECT COUNT( * ) FROM $table_name";
 			$existing_payment_methods = $wpdb->get_var($SQL);
-			$default_admin_only_payment_methods = apply_filters( 
-					'FHEE__EEH_Activation__add_default_admin_only_payments__default_admin_only_payment_methods', 
+			$default_admin_only_payment_methods = apply_filters(
+					'FHEE__EEH_Activation__add_default_admin_only_payments__default_admin_only_payment_methods',
 					array(
 					__("Bank", 'event_espresso')=>  __("Bank Draft", 'event_espresso'),
 					__("Cash", 'event_espresso')=>  __("Cash Delivered Physically", 'event_espresso'),
