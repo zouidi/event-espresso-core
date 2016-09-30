eab8e9ec606a30cf28375946858dbddc32d9fa57
Brent Christensen <brent@eventespresso.com>
2016-09-30 10:16:32 -0700

    generic interface for classes that need to return a CapCheck DTO; [touch:10120]

---
+++ core/domain/services/capabilities/RequiresCapCheckInterface.php
@@ -0,0 +1,26 @@
+<?php
+namespace EventEspresso\core\domain\services\capabilities;
+
+
+defined( 'ABSPATH' ) || exit;
+
+
+
+/**
+ * Interface RequiresCapCheckInterface
+ * this interface is used to identify classes that
+ * require a permissions check before they can be executed.
+ *
+ * @package EventEspresso\core\domain\services\capabilities
+ */
+interface RequiresCapCheckInterface
+{
+
+    /**
+     * @return CapCheckInterface
+     */
+    public function getCapCheck();
+
+}
+// End of file RequiresCapCheckInterface.php
+// Location: EventEspresso\core\domain\services\capabilities/RequiresCapCheckInterface.php
