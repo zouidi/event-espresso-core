09f2fffa6bd5d7329fe49fa26f5a6c19a8273a22
Brent Christensen <brent@eventespresso.com>
2016-12-16 09:27:27 -0800

    RequiresCapCheckInterface; [touch:10317]

---
+++ core/services/automated_actions/TriggerStrategyHourly.php
@@ -0,0 +1,34 @@
+<?php
+namespace EventEspresso\core\services\automated_actions;
+
+defined('ABSPATH') || exit;
+
+
+
+/**
+ * Class TriggerStrategyHourly
+ * Description
+ *
+ * @package       Event Espresso
+ * @author        Brent Christensen
+ * @since         $VID:$
+ */
+class TriggerStrategyHourly extends TriggerStrategyCron
+{
+
+
+
+    /**
+     * when a trigger has been "pulled",
+     * either by a do_action or scheduled cron,
+     * this is the callback method that will run
+     * to actually execute the logic
+     * for the automated action
+     */
+    public function execute()
+    {
+        // TODO: Implement execute() method.
+    }
+}
+// End of file TriggerStrategyHourly.php
+// Location: EventEspresso\core\services\automated_actions/TriggerStrategyHourly.php
