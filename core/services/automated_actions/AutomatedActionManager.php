7b07d81a7e99b2a3777ef82f952c16fcd82beb45
Brent Christensen <brent@eventespresso.com>
2016-12-16 09:25:32 -0800

    updates to work with new classes; [touch:10317]

--- core/services/automated_actions/AutomatedActionManager.php
+++ core/services/automated_actions/AutomatedActionManager.php
@@ -2,12 +2,16 @@
 namespace EventEspresso\core\services\automated_actions;
 
 use EventEspresso\core\domain\services\capabilities\CapabilitiesChecker;
-// use EventEspresso\core\domain\services\capabilities\RequiresCapCheckInterface;
+use EventEspresso\core\domain\services\capabilities\RequiresCapCheckInterface;
+use EventEspresso\core\exceptions\InsufficientPermissionsException;
+use EventEspresso\core\exceptions\InvalidClassException;
+use EventEspresso\core\exceptions\InvalidEntityException;
+use EventEspresso\core\exceptions\InvalidInterfaceException;
+use EventEspresso\core\services\Benchmark;
 use EventEspresso\core\services\collections\Collection;
-use EventEspresso\core\services\collections\CollectionDetails;
-use EventEspresso\core\services\collections\CollectionLoader;
+use stdClass;
 
-defined('ABSPATH') || exit;
+defined('EVENT_ESPRESSO_VERSION') || exit;
 
 
 
@@ -36,77 +40,121 @@ class AutomatedActionManager
 
 
     /**
-     * CapChecker constructor
+     * AutomatedActionManager constructor
      *
      * @param CapabilitiesChecker $capabilities_checker
+     * @param Collection          $automated_actions
+     * @throws InvalidInterfaceException
      */
-    public function __construct(CapabilitiesChecker $capabilities_checker)
+    public function __construct(CapabilitiesChecker $capabilities_checker, Collection $automated_actions = null)
     {
+        Benchmark::startTimer(__METHOD__);
         $this->capabilities_checker = $capabilities_checker;
+        $this->automated_actions    = $automated_actions instanceof Collection
+            ? $automated_actions
+            : new Collection('EventEspresso\core\services\automated_actions\AutomatedActionInterface');
+        add_action('AHEE__EE_System__initialize', array($this, 'initialize'));
+        Benchmark::stopTimer(__METHOD__);
     }
 
 
 
     /**
-     * @return Collection
-     * @throws \EventEspresso\core\exceptions\InvalidIdentifierException
-     * @throws \EventEspresso\core\exceptions\InvalidInterfaceException
-     * @throws \EventEspresso\core\exceptions\InvalidFilePathException
-     * @throws \EventEspresso\core\exceptions\InvalidEntityException
-     * @throws \EventEspresso\core\exceptions\InvalidDataTypeException
-     * @throws \EventEspresso\core\exceptions\InvalidClassException
+     * @throws \DomainException
+     * @throws InvalidEntityException
+     * @throws InsufficientPermissionsException
+     * @throws InvalidClassException
      */
-    protected function loadAutomatedActionCollection()
+    public function initialize()
     {
-        if ( ! $this->automated_actions instanceof Collection) {
-            $loader = new CollectionLoader(
-                new CollectionDetails(
-                // collection name
-                    'automated_actions',
-                    // collection interface
-                    '\EventEspresso\core\services\automated_actions\AutomatedActionInterface',
-                    // FQCNs for classes to add (all classes within that namespace will be loaded)
-                    array('EventEspresso\core\domain\services\automated_actions'),
-                    // filepaths to classes to add
-                    array(),
-                    // filemask to use if parsing folder for files to add
-                    '',
-                    // what to use as identifier for collection entities
-                    // using CLASS NAME prevents duplicates (works like a singleton)
-                    CollectionDetails::ID_CLASS_NAME
-                )
-            );
-            $this->automated_actions = $loader->getCollection();
-        }
-        return $this->automated_actions;
+        $results = $this->getActiveActions();
+        $this->generateActionObjects($results);
+        $this->setTriggers();
+        add_action('shutdown', array($this, 'processActions'));
     }
 
 
 
-    public function setConditionCheckHooks()
+    /**
+     * @return array
+     * @throws \DomainException
+     */
+    protected function getActiveActions()
     {
-        foreach ($this->loadAutomatedActionCollection() as $action_item) {
-            /** @var AutomatedActionInterface $action_item */
-            if ($action_item instanceof RequiresCapCheckInterface) {
+        Benchmark::startTimer(__METHOD__);
+        global $wpdb;
+        $results = $wpdb->get_results(
+            "SELECT * FROM {$wpdb->prefix}esp_automated_action WHERE AMA_is_active = 1;"
+        );
+        if ($results instanceof \WP_Error) {
+            throw new \DomainException(
+                $results->get_error_message()
+            );
+        }
+        $results = is_array($results) ? $results : array($results);
+        Benchmark::stopTimer(__METHOD__);
+        return $results;
+    }
+
+
+
+    /**
+     * @param array $results
+     * @throws InvalidEntityException
+     * @throws \DomainException
+     */
+    protected function generateActionObjects(array $results)
+    {
+        Benchmark::startTimer(__METHOD__);
+        foreach ($results as $result) {
+            $this->automated_actions->add(
+                AutomatedActionFactory::create($result),
+                $result->AMA_name
+            );
+        }
+        Benchmark::stopTimer(__METHOD__);
+    }
+
+
+
+    /**
+     * @throws InsufficientPermissionsException
+     * @throws InvalidClassException
+     */
+    public function setTriggers()
+    {
+        \EEH_Debug_Tools::printr(__FUNCTION__, __CLASS__, __FILE__, __LINE__, 2);
+        Benchmark::startTimer(__METHOD__);
+        foreach ($this->automated_actions as $automated_action) {
+            /** @var AutomatedActionInterface $automated_action */
+            if ($automated_action instanceof RequiresCapCheckInterface) {
                 $this->capabilities_checker->processCapCheck(
-                    $action_item->getCapCheck()
+                    $automated_action->getCapCheck()
                 );
             }
-            $action_item->setConditionCheckHooks();
+            $automated_action->setTrigger();
         }
+        Benchmark::stopTimer(__METHOD__);
     }
 
 
 
-    public function processConditionChecks()
+    /**
+     *
+     */
+    public function processActions()
     {
-        foreach ($this->loadAutomatedActionCollection() as $action_item) {
-            /** @var AutomatedActionInterface $action_item */
-            if ( ! $action_item->conditionPassed()) {
+        \EEH_Debug_Tools::printr(__FUNCTION__, __CLASS__, __FILE__, __LINE__, 2);
+        Benchmark::startTimer(__METHOD__);
+        foreach ($this->automated_actions as $automated_action) {
+            /** @var AutomatedActionInterface $automated_action */
+            if ( ! $automated_action->triggerPulled() || $automated_action->hasRun()) {
                 continue;
             }
-
+            $automated_action->process();
         }
+        Benchmark::stopTimer(__METHOD__);
+        Benchmark::displayResults();
     }
 
 
