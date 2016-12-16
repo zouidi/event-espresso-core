b5afc7ef32c92c76a6eedcec4ae4a8dd171524a1
Brent Christensen <brent@eventespresso.com>
2016-12-16 09:33:17 -0800

    initial version of Class modelObject, abstract RuleStrategy base class for translating EE model object related Rules into query params [touch:10317]

---
+++ core/services/conditional_logic/rules/modelObject.php
@@ -0,0 +1,24 @@
+<?php
+namespace EventEspresso\core\services\conditional_logic\rules;
+
+defined('ABSPATH') || exit;
+
+
+
+/**
+ * Class modelObject
+ * abstract RuleStrategy base class for translating EE model object related Rules into query params
+ *
+ * @package       Event Espresso
+ * @author        Brent Christensen
+ * @since         $VID:$
+ */
+class modelObject
+{
+
+    protected $object;
+
+
+}
+// End of file modelObject.php
+// Location: EventEspresso\core\services\conditional_logic\rules/modelObject.php
