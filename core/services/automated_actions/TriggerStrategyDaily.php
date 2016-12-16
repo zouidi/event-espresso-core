55561f8485da399f6e3378e3af2123ecd6fb0244
Brent Christensen <brent@eventespresso.com>
2016-12-16 09:27:13 -0800

    initial version of TriggerStrategyDaily; [touch:10317]

---
+++ core/services/automated_actions/TriggerStrategyDaily.php
@@ -0,0 +1,34 @@
+<?php
+namespace EventEspresso\core\services\automated_actions;
+
+defined('ABSPATH') || exit;
+
+
+
+/**
+ * Class TriggerStrategyDaily
+ * Description
+ *
+ * @package       Event Espresso
+ * @author        Brent Christensen
+ * @since         $VID:$
+ */
+class TriggerStrategyDaily extends TriggerStrategyCron
+{
+
+
+
+    /**
+     * when a trigger has been "pulled",
+     * either by a do_action or scheduled cron,
+     * this is the callback method that will run
+     * to actually execute the logic
+     * for the automated action
+     */
+    public function execute()
+    {
+        // TODO: Implement execute() method.
+    }
+}
+// End of file TriggerStrategyDaily.php
+// Location: EventEspresso\core\services\automated_actions/TriggerStrategyDaily.php
