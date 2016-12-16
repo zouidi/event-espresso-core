b8322960f2b34afe161b34e5b6c366214ef04364
Brent Christensen <brent@eventespresso.com>
2016-12-16 09:22:01 -0800

    initial version of AutomatedActionInterface (includes debug code); [touch:10317]

---
+++ core/services/automated_actions/AutomatedActionInterface.php
@@ -0,0 +1,48 @@
+<?php
+namespace EventEspresso\core\services\automated_actions;
+
+defined('ABSPATH') || exit;
+
+
+
+interface AutomatedActionInterface
+{
+
+    /**
+     * @return int
+     */
+    public function getID();
+
+    /**
+     * @return string
+     */
+    public function getTriggerValue();
+
+    /**
+     * @return \stdClass
+     */
+    public function getData();
+
+    /**
+     * @return void
+     */
+    public function setTrigger();
+
+    /**
+     * @return bool
+     */
+    public function triggerPulled();
+
+    /**
+     * @return bool
+     */
+    public function hasRun();
+
+    /**
+     * @return void
+     */
+    public function process();
+
+}
+// End of file AutomatedActionInterface.php
+// Location: EventEspresso\core\services\automated_actions/AutomatedActionInterface.php
