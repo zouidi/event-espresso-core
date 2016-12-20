b569f7f30c5c48df16af6effdcc2f3c94f801b55
Brent Christensen <brent@eventespresso.com>
2016-12-20 15:28:15 -0800

    remove execute logic unless override is needed; [touch:10317]

--- core/services/automated_actions/TriggerStrategyDate.php
+++ core/services/automated_actions/TriggerStrategyDate.php
@@ -18,17 +18,7 @@ class TriggerStrategyDate extends TriggerStrategyCron
 
 
 
-    /**
-     * when a trigger has been "pulled",
-     * either by a do_action or scheduled cron,
-     * this is the callback method that will run
-     * to actually execute the logic
-     * for the automated action
-     */
-    public function execute(array $query_params)
-    {
-        // TODO: Implement execute() method.
-    }
+
 }
 // End of file TriggerStrategyDate.php
 // Location: EventEspresso\core\services\automated_actions/TriggerStrategyDate.php
