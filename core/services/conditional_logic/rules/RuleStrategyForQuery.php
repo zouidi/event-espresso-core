ee371b1034da17c22e6d8de266f7e6be2a870266
Brent Christensen <brent@eventespresso.com>
2016-12-20 15:35:48 -0800

    abstract RuleStrategy class for translating EE model related Rules into query params; [touch:10317]

---
+++ core/services/conditional_logic/rules/RuleStrategyForQuery.php
@@ -0,0 +1,23 @@
+<?php
+namespace EventEspresso\core\services\conditional_logic\rules;
+
+defined('EVENT_ESPRESSO_VERSION') || exit;
+
+
+
+/**
+ * Class RuleStrategyForQuery
+ * abstract RuleStrategy class for translating EE model related Rules into query params
+ *
+ * @package       Event Espresso
+ * @author        Brent Christensen
+ * @since         $VID:$
+ */
+abstract class RuleStrategyForQuery extends RuleStrategy
+{
+
+
+
+}
+// End of file RuleStrategyForQuery.php
+// Location: EventEspresso\core\services\conditional_logic\rules/RuleStrategyForQuery.php
