bd231bad6f3ac893b1accfe9b1373b021b1fd260
Lorenzo Orlando Caum <hello@lorenzocaum.com>
2016-12-21 06:58:41 -0500

    Update messaging on messaging upgrade for custom message templates. [touch:10325]

--- admin_pages/messages/Messages_Admin_Page.core.php
+++ admin_pages/messages/Messages_Admin_Page.core.php
@@ -1012,7 +1012,7 @@ class Messages_Admin_Page extends EE_Admin_Page
         $this->_admin_page_title              = __('Custom Message Templates (Preview)', 'event_espresso');
         $this->_template_args['preview_img']  = '<img src="' . EE_MSG_ASSETS_URL . 'images/custom_mtps_preview.png" alt="' . esc_attr__('Preview Custom Message Templates screenshot',
                 'event_espresso') . '" />';
-        $this->_template_args['preview_text'] = '<strong>' . __('Custom Message Templates is a feature that is only available in the Event Espresso 4. With the Custom Message Templates feature, you are able to create custom templates and set them per event.',
+        $this->_template_args['preview_text'] = '<strong>' . __('Custom Message Templates is a feature that is only available in Event Espresso 4. With the Custom Message Templates feature, you are able to create custom templates and set them per event.',
                 'event_espresso') . '</strong>';
         $this->display_admin_caf_preview_page('custom_message_types', false);
     }
