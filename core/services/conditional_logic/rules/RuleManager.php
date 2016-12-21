58815bc642e94e952a8d6e43b5eb5b3eff338304
Brent Christensen <brent@eventespresso.com>
2016-12-20 17:08:43 -0800

    add constants for rule type; reorder methods; pass rule type to generateQueryParams(); [touch:10317]

--- core/services/conditional_logic/rules/RuleManager.php
+++ core/services/conditional_logic/rules/RuleManager.php
@@ -23,6 +23,9 @@ defined('EVENT_ESPRESSO_VERSION') || exit;
 class RuleManager
 {
 
+    const RULE_TYPE_QUERY = 'query';
+    const RULE_TYPE_OBJECT = 'object';
+
     /**
      * @var QueryParamGenerator $query_generator
      */
@@ -39,6 +42,61 @@ class RuleManager
     public function __construct(QueryParamGenerator $query_generator)
     {
         $this->query_generator = $query_generator;
+    }
+
+
+
+    /**
+     * @param \stdClass[] $results
+     * @return Collection
+     * @throws InvalidInterfaceException
+     * @throws InvalidArgumentException
+     * @throws InvalidEntityException
+     */
+    protected function getRulesCollection(array $results)
+    {
+        Benchmark::startTimer(__METHOD__);
+        $rules = new Collection('EventEspresso\core\services\conditional_logic\rules\Rule');
+        foreach ($results as $result) {
+            $rules->add(new Rule($result));
+        }
+        Benchmark::stopTimer(__METHOD__);
+        return $rules;
+    }
+
+
+
+    /**
+     * @param string $OBJ_name
+     * @param mixed  $OBJ_ID
+     * @return array
+     * @throws DomainException
+     */
+    public function retrieveRules($OBJ_name, $OBJ_ID)
+    {
+        Benchmark::startTimer(__METHOD__);
+        global $wpdb;
+        $results = $wpdb->get_results(
+            $wpdb->prepare(
+                "SELECT * FROM {$wpdb->prefix}esp_object_rule AS object_rule
+                 LEFT JOIN {$wpdb->prefix}esp_rule AS rule
+                 ON object_rule.RUL_ID = rule.RUL_ID
+                 WHERE object_rule.ORL_OBJ_name = %s
+                 AND object_rule.ORL_OBJ_ID = %d
+                 ORDER BY object_rule.ORL_order",
+                $OBJ_name,
+                $OBJ_ID
+            )
+        );
+        \EEH_Debug_Tools::printr($results, '$results', __FILE__, __LINE__);
+        if ($results instanceof \WP_Error) {
+            throw new DomainException(
+                $results->get_error_message()
+            );
+        }
+        $results = is_array($results) ? $results : array($results);
+        Benchmark::stopTimer(__METHOD__);
+        return $results;
     }
 
 
@@ -82,68 +140,13 @@ class RuleManager
 
 
     /**
-     * @param string $OBJ_name
-     * @param mixed  $OBJ_ID
-     * @return array
-     * @throws DomainException
-     */
-    public function retrieveRules( $OBJ_name, $OBJ_ID )
-    {
-        Benchmark::startTimer(__METHOD__);
-        global $wpdb;
-        $results = $wpdb->get_results(
-            $wpdb->prepare(
-                "SELECT * FROM {$wpdb->prefix}esp_object_rule AS object_rule
-                 LEFT JOIN {$wpdb->prefix}esp_rule AS rule
-                 ON object_rule.RUL_ID = rule.RUL_ID
-                 WHERE object_rule.ORL_OBJ_name = %s
-                 AND object_rule.ORL_OBJ_ID = %d
-                 ORDER BY object_rule.ORL_order",
-                $OBJ_name,
-                $OBJ_ID
-            )
-        );
-        \EEH_Debug_Tools::printr($results, '$results', __FILE__, __LINE__);
-        if ($results instanceof \WP_Error) {
-            throw new DomainException(
-                $results->get_error_message()
-            );
-        }
-        $results = is_array($results) ? $results : array($results);
-        Benchmark::stopTimer(__METHOD__);
-        return $results;
-    }
-
-
-
-    /**
-     * @param \stdClass[] $results
-     * @return Collection
-     * @throws InvalidInterfaceException
-     * @throws InvalidArgumentException
-     * @throws InvalidEntityException
-     */
-    protected function getRulesCollection(array $results)
-    {
-        Benchmark::startTimer(__METHOD__);
-        $rules = new Collection('EventEspresso\core\services\conditional_logic\rules\Rule');
-        foreach ($results as $result) {
-            $rules->add(new Rule($result));
-        }
-        Benchmark::stopTimer(__METHOD__);
-        return $rules;
-    }
-
-
-
-    /**
      * @param Collection $rules
      * @return array
      */
     public function getQueryParamsForRules(Collection $rules)
     {
         $this->query_generator->addRules($rules);
-        $SQL = $this->query_generator->generateQueryParams();
+        $SQL = $this->query_generator->generateQueryParams(RuleManager::RULE_TYPE_QUERY);
         \EEH_Debug_Tools::printr($SQL, '$SQL', __FILE__, __LINE__);
         return $SQL;
     }
