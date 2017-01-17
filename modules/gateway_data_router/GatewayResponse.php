b7e4af6e59a592f8be8e6324fb6901a4a210dfec
Brent Christensen <brent@eventespresso.com>
2017-01-17 12:48:45 -0800

    update references to moved constants and properties; use use; apply PSR formatting; [touch:9334]

--- modules/gateway_data_router/GatewayResponse.php
+++ modules/gateway_data_router/GatewayResponse.php
@@ -2,58 +2,57 @@
 namespace EventEspresso\modules\gateway_data_router;
 
 use EventEspresso\modules\ForwardedModuleResponse;
+use EventEspresso\modules\InvalidModuleResponseDataException;
 
-if ( ! defined( 'EVENT_ESPRESSO_VERSION' ) ) {
-	exit( 'No direct script access allowed' );
-}
+defined('EVENT_ESPRESSO_VERSION') || exit();
 
 
 
 /**
  * Class GatewayResponse
- *
  * Description
  *
  * @package       Event Espresso
  * @subpackage    core
  * @author        Brent Christensen
  * @since         $VID:$
- *
  */
-class GatewayResponse extends ForwardedModuleResponse {
+class GatewayResponse extends ForwardedModuleResponse
+{
 
-	/**
-	 * GatewayResponse constructor.
-	 */
-	public function __construct(
-	) {
-		// plz see the ForwardedModuleResponse::$dataMap description for details regarding this
-		$this->setDataMap(
-			array(
-				'session_id'                 => array( 'sanitize_text_field' ),
-				'transaction_id'             => array( 'absint' ),
-				'transaction'                => array( 'instanceof' => 'EE_Transaction' ),
-				'selected_method_of_payment' => array( 'sanitize_text_field' ),
-			)
-		);
-		parent::__construct();
-	}
+    /**
+     * GatewayResponse constructor.
+     *
+     * @throws InvalidModuleResponseDataException
+     */
+    public function __construct()
+    {
+        // plz see the ForwardedModuleResponse::$data_map description for details regarding this
+        $this->setDataMap(
+            array(
+                'session_id'                 => array('sanitize_text_field'),
+                'transaction_id'             => array('absint'),
+                'transaction'                => array('instanceof' => 'EE_Transaction'),
+                'selected_method_of_payment' => array('sanitize_text_field'),
+            )
+        );
+        parent::__construct();
+    }
 
 
 
-	/**
-	 * overrides the parent valid() method which merely checks that the data array is not empty,
-	 * but we want to ensure that these critical parameters are set before forwarding this object
-	 *
-	 * @return bool
-	 */
-	public function valid() {
-		return ! empty( $this->data['session_id'] )
-		       && ! empty( $this->data[ 'transaction_id' ] )
-		       && ! empty( $this->data[ 'selected_method_of_payment' ] )
-			? true
-			: false;
-	}
+    /**
+     * overrides the parent valid() method which merely checks that the data array is not empty,
+     * but we want to ensure that these critical parameters are set before forwarding this object
+     *
+     * @return bool
+     */
+    public function valid()
+    {
+        return ! empty($this->data['session_id'])
+               && ! empty($this->data['transaction_id'])
+               && ! empty($this->data['selected_method_of_payment']);
+    }
 
 
 }
