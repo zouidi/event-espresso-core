aa066e7837656824397f264e37fc6f7230b69bc0
Brent Christensen <brent@eventespresso.com>
2017-01-24 21:12:15 -0800

    add interface for JobScheduler classes like CronManager; [touch:10317]

---
+++ core/services/automated_actions/JobSchedulerInterface.php
@@ -0,0 +1,13 @@
+<?php
+namespace EventEspresso\core\services\automated_actions;
+
+defined('EVENT_ESPRESSO_VERSION') || exit;
+
+
+
+interface JobSchedulerInterface
+{
+
+}
+// End of file JobSchedulerInterface.php
+// Location: EventEspresso\core\services\automated_actions/JobSchedulerInterface.php
