c39cf8f9f837757a15aa308fbebd2b1e7204a98d
Brent Christensen <brent@eventespresso.com>
2017-01-24 21:36:43 -0800

    add JobInterface; [touch:10317]

---
+++ core/services/automated_actions/JobInterface.php
@@ -0,0 +1,13 @@
+<?php
+namespace EventEspresso\core\services\automated_actions;
+
+defined('EVENT_ESPRESSO_VERSION') || exit;
+
+
+
+interface JobInterface
+{
+
+}
+// End of file JobInterface.php
+// Location: EventEspresso\core\services\automated_actions/JobInterface.php
