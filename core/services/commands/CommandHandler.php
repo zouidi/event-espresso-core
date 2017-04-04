74ddb4575a31a2311ab596acf4515a56e8045cd0
Brent Christensen <brent@eventespresso.com>
2017-04-04 15:55:19 -0700

    add validateArrayElement() helper method to parent CommandHandler; [touch:10617]

--- core/services/commands/CommandHandler.php
+++ core/services/commands/CommandHandler.php
@@ -1,6 +1,8 @@
 <?php
 namespace EventEspresso\core\services\commands;
 
+use InvalidArgumentException;
+
 if ( ! defined('EVENT_ESPRESSO_VERSION')) {
     exit('No direct script access allowed');
 }
@@ -9,7 +11,7 @@ if ( ! defined('EVENT_ESPRESSO_VERSION')) {
 
 /**
  * Class CommandHandler
- * Description
+ * abstract parent class for CommandHandlers
  *
  * @package       Event Espresso
  * @author        Brent Christensen
@@ -17,6 +19,36 @@ if ( ! defined('EVENT_ESPRESSO_VERSION')) {
  */
 abstract class CommandHandler implements CommandHandlerInterface
 {
+
+
+
+    /**
+     * ensures an array element has an appropriate value
+     *
+     * @param array      $data
+     * @param int|string $key
+     * @param null       $default
+     * @param bool       $required
+     * @return mixed|null
+     * @throws InvalidArgumentException
+     */
+    protected function validateArrayElement(array $data, $key, $default = null, $required = false)
+    {
+        if (empty($data[$key])) {
+            if ($required) {
+                throw new InvalidArgumentException(
+                    sprintf(
+                        esc_html__('"%1$s" is a required value and must be set.', 'event_espresso'),
+                        $key
+                    )
+                );
+            }
+            return $default;
+        }
+        return $data[$key];
+    }
+
+
 
 }
 // End of file CommandHandler.php
